a90941bcbcd07e56c74f73e7a4260a6cc064813b
Illia Solovei <illia.solovei@deutschepost.de>
2024-09-05 13:37:18 +0200

    Add plugin folder to uilib

---
+++ packages/uilib/src/lib/plugins/type-designer/type-designer.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import { SvelteFlowProvider } from '@xyflow/svelte';
+	import type { Networking } from "@oscd-plugins/core";
+	import Theme from "../../theme/theme.svelte"
+
+	// 
+	// INPUT
+	// 
+	export let root: Element
+	export let editCount: number
+
+	// 
+	// INTERNAL
+	// 
+	let htmlRoot: HTMLElement
+</script>
+
+<Theme>
+	<SvelteFlowProvider>
+	<type-designer bind:this={htmlRoot}>
+		<p>Type Designer - Hello</p>
+	</type-designer>
+	</SvelteFlowProvider>
+</Theme>
+
+<style>
+	:root, :host {
+		--header-height: 128px;
+	}
+	type-designer {
+		height: calc(100vh - var(--header-height));;
+		display: flex;
+ 	 	align-items: stretch;
+		position: relative;
+		/* font-size: 12px; */
+	}
+</style>
