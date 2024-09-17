0ece8ee6a5b3e283d2eed3880d17df5a7695ca73
Illia Solovei <illia.solovei@deutschepost.de>
2024-09-17 02:36:25 +0200

    feat: add control buttons and remaining search queries
    (dirty version)
    - add remaining search queries, their wrappers and types
    - complete the prototype version of showing all variations of types available
    - add buttons to add new types (inactive)
    - small refactor of bay component

---
+++ packages/uilib/src/lib/plugins/type-designer/components/add-component-controls.svelte
@@ -0,0 +1,23 @@
+<script>
+  import Button from "@smui/button";
+</script>
+
+<div class="container">
+  <Button>Add LN</Button>
+  <Button>Add LD</Button>
+  <Button>Add IED</Button>
+  <Button>Add Bay</Button>
+</div>
+
+<style>
+    .container {
+      position: fixed;
+      bottom: 0;
+      right: 0;
+      display: flex;
+      flex-direction: column;
+      align-items: flex-end;
+      gap: 8px;
+      padding: 16px;
+    }
+</style>
