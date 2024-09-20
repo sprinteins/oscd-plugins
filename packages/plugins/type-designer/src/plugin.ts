53e3b3d3f611492555bb132e72758c3772894470
Illia Solovei <i.solovei@sprinteins.com>
2024-09-20 12:15:11 +0200

    feat: real-time doc update on createNode + fix wrong names in scd

--- packages/plugins/type-designer/src/plugin.ts
+++ packages/plugins/type-designer/src/plugin.ts
@@ -10,21 +10,31 @@ export default class NewPlugin extends HTMLElement {
 		this.plugin = new Plugin({
 			target: this.shadowRoot,
 			props: {
-				doc: this._doc
+				doc: this._doc,
+				editCount: -1
 			}
 		});
 
 		const style = document.createElement("style");
         style.innerHTML = globalThis.pluginStyle[pkg.name];
-        this.shadowRoot.appendChild(style);
+        this.shadowRoot!.appendChild(style);
 	}
 
 	private _doc: XMLDocument
 	public set doc(newDoc: XMLDocument){
 		this._doc = newDoc
-		if(!this.plugin) { return }
+		if(!this.plugin) {
+			return
+		}
 
 		this.plugin.$set({doc: newDoc})
+	}
+
+	public set editCount(newCount: number) {
+		if (!this.plugin) {
+			return
+		}
+		this.plugin.$set({ editCount: newCount })
 	}
 
 }
