fa5b319dbb7bbdacb0ec5a0769d80fa1f5057687
Sergio Alvarenga <s.alvarenga@sprinteins.com>
2024-10-14 11:26:27 +0200

    create standalone text and img element

---
+++ packages/uilib/src/lib/plugins/auto-doc/elements/index.ts
@@ -0,0 +1,2 @@
+export { default as ImageElement } from "./image/image-element.svelte"
+export { default as TextElement } from "./text/text-element.svelte"
---
+++ packages/uilib/src/lib/plugins/auto-doc/elements/text/text-element.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	export let content = "";
+</script>
+
+
+<div>
+	<textarea 
+		name="templateText"
+		id=""
+		bind:value={content}
+		placeholder="Enter your content here"
+		/>
+</div>
+
+<style>
+
+	textarea {
+		width: 100%;
+		padding: 0.5rem;
+		font-size: 1rem;
+	}
+</style>
