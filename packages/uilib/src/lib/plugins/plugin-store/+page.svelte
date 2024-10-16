60419045a1cc1cfaa347715fb3b7fb2e043823dc
Rasul Adasov <rasul@futureblur.com>
2024-10-16 10:23:49 +0200

    fix: example not displaying correctly

--- packages/uilib/src/lib/plugins/plugin-store/+page.svelte
+++ packages/uilib/src/lib/plugins/plugin-store/+page.svelte
@@ -1,12 +1,10 @@
 <script lang="ts">
 import { PluginStore } from '.'
-import { xmlStr } from '../../../test-files/simple_v5'
 import { Example } from '../../components/internal'
 
-const parser = new DOMParser()
-const doc = parser.parseFromString(xmlStr, 'text/xml') as unknown as Element
+let isOpen = true
 </script>
 
 <Example name="PluginStore">
-    <PluginStore root={doc} />
+    <PluginStore bind:isOpen />
 </Example>
