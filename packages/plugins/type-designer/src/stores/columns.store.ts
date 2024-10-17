72e5162954aeff2cecd30fbcfa54a1e1b896023f
Illia Solovei <i.solovei@sprinteins.com>
2024-10-17 07:57:16 +0200

    feat: expose columns API via its new store service

---
+++ packages/plugins/type-designer/src/stores/columns.store.ts
@@ -0,0 +1,62 @@
+import { writable } from "svelte/store";
+import { ELEMENT_NAMES } from "@oscd-plugins/core";
+import type { DataTypeTemplatesService } from "@oscd-plugins/core";
+import type { Column } from "../components/elements-type-container"
+import { createNewItem } from "@/components/elements-type-container/create-new-item";
+
+export const ColumnsStore = (service: DataTypeTemplatesService) => {
+  const typeCluster = service.findTypeDesignerElements();
+
+  const initialColumns: Column[] = [
+    {
+      name: ELEMENT_NAMES.substation,
+      visible: true,
+      items: typeCluster.substations,
+    },
+    {
+      name: ELEMENT_NAMES.voltageLevel,
+      visible: true,
+      items: typeCluster.voltageLevels,
+    },
+    { name: ELEMENT_NAMES.bay, visible: true, items: typeCluster.bays },
+    { name: ELEMENT_NAMES.ied, visible: true, items: typeCluster.ieds },
+    {
+      name: ELEMENT_NAMES.lDevice,
+      visible: true,
+      items: typeCluster.logicalDevices,
+    },
+    { name: ELEMENT_NAMES.lNode, visible: true, items: [] },
+  ];
+
+  const { subscribe, update } = writable<Column[]>(initialColumns);
+
+  const toggleColumnVisibility = (index: number) => {
+    update((columns) =>
+      columns.map((column, i) =>
+        i === index ? { ...column, visible: !column.visible } : column
+      )
+    );
+  };
+
+  const addItemToColumn = (index: number) => {
+    update((columns) => {
+      const column = columns[index];
+      const itemCount = column.items.length;
+      const newItem = createNewItem(column.name, itemCount);
+
+      if (newItem) {
+        return columns.map((col, i) =>
+          i === index ? { ...col, items: [...col.items, newItem] } : col
+        );
+      }
+
+      return columns;
+    });
+  };
+
+  return {
+    subscribe,
+    toggleColumnVisibility,
+    addItemToColumn,
+  };
+};
