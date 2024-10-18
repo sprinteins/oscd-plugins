31887a534223bdde35ed66cd6b42393c6542e082
Felix Wagner <f.wagner@sprinteins.com>
2024-10-18 10:26:51 +0200

    add vitest with playwright

---
+++ packages/plugins/auto-doc/tests/setupTests.ts
@@ -0,0 +1,27 @@
+import { beforeAll, afterAll, test, expect } from 'vitest';
+import { chromium } from 'playwright';
+import type { Browser, BrowserContext, Page } from 'playwright';
+
+declare global {
+  var browser: Browser;
+  var context: BrowserContext;
+  var page: Page;
+}
+
+beforeAll(async () => {
+  globalThis.browser = await chromium.launch();
+  globalThis.context = await browser.newContext();
+  globalThis.page = await context.newPage();
+});
+
+afterAll(async () => {
+  await globalThis.page.close();
+  await globalThis.context.close();
+  await globalThis.browser.close();
+});
+
+test('sample test', async () => {
+  await page.goto('https://example.com');
+  const title = await page.title();
+  expect(title).toBe('Example Domain');
+});
