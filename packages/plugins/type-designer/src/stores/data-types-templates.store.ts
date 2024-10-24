24f9645971b7f2ff1478fc18655b87fcb1b2cc5b
Michel Guerin <dev@guerin.at>
2024-10-24 15:24:01 +0200

    feat(type-designer): rewrite of all stores to handle data persistance
    Define clear scope betweem stores :
    * plugin store handles the stock and standard events (facing the open scd instance)
    * columns store handles the interactions for the columns and upcoming cards
    * data-type-templates store contains the business logic of the plugin (interaction with the SCD file)

--- packages/plugins/type-designer/src/stores/data-types-templates.store.ts
+++ packages/plugins/type-designer/src/stores/data-types-templates.store.ts
@@ -1,43 +1,161 @@
 import { writable, get } from 'svelte/store'
 // CORE
-import { DataTypeTemplatesService } from '@oscd-plugins/core'
+import { DataTypeTemplatesService, SCD_ELEMENTS } from '@oscd-plugins/core'
+// STORES
+import { pluginStore } from './plugin.store'
 // TYPES
-import type { DataTypeTemplates } from '@oscd-plugins/core'
+import type { PrivateElement, DataTypeTemplates } from '@oscd-plugins/core'
+
+//==== INITIALIZER
+
+const typeElementsInitializer: DataTypeTemplates.TypeElements = {
+	substationTypes: [],
+	voltageLevelTypes: [],
+	bayTypes: [],
+	iedTypes: [],
+	lDeviceTypes: [],
+	lNodeTypes: []
+}
 
 //==== STATE
+const dataTypeTemplateService = writable<DataTypeTemplatesService>()
 const rootElement = writable<DataTypeTemplates.RootElement | null>(null)
-const subElements = writable<DataTypeTemplates.SubElements>()
+const privateElement = writable<PrivateElement | null>(null)
+const typeElements = writable<DataTypeTemplates.TypeElements>(
+	typeElementsInitializer
+)
+const isStructureCreated = writable(false)
 
 //==== PRIVATE ACTIONS
 
-function setRootElement(newXmlDocument: Element) {
-	const typeDesigner = new DataTypeTemplatesService(newXmlDocument)
-	const newRootElement = typeDesigner.findDataTypesElement()
+function setDataTypesTemplateService(newXmlDocument: XMLDocument) {
+	dataTypeTemplateService.set(
+		new DataTypeTemplatesService(newXmlDocument.documentElement)
+	)
+}
 
+function setRootElement() {
+	const newRootElement = get(dataTypeTemplateService).findDataTypesElement()
 	rootElement.set(newRootElement)
 }
 
-function setSubElements(newXmlDocument: Element) {
-	const typeDesigner = new DataTypeTemplatesService(newXmlDocument)
+function setPrivateElement() {
+	const currentRootElement = get(rootElement)
+	if (!currentRootElement) return privateElement.set(null)
 
-	const newSubElements = typeDesigner.findTypeDesignerElements({
-		root: get(rootElement)?.element
+	const newPrivateElement = get(dataTypeTemplateService).findPrivateElement(
+		'[type="type-designer-plugin"]',
+		{
+			root: currentRootElement.element
+		}
+	)
+
+	privateElement.set(newPrivateElement)
+}
+
+function setTypeElements() {
+	const currentPrivateElement = get(privateElement)
+
+	const newSubElements = get(dataTypeTemplateService).findTypeElements({
+		root: currentPrivateElement?.element
 	})
 
-	subElements.set(newSubElements)
+	typeElements.set(newSubElements)
 }
 
-//==== INITIALIZATION
-function init(newXmlDocument: Element | undefined) {
+function createDataTypeTemplateElement() {
+	const currentXmlDocumentElement = get(
+		pluginStore.xmlDocument
+	)?.documentElement
+	if (!currentXmlDocumentElement)
+		return console.error('No XML document found')
+
+	const newDataTypeTemplatesElement = pluginStore.addElementToXmlDocument({
+		parentElement: currentXmlDocumentElement,
+		newElementTagName: 'DataTypeTemplates'
+	})
+	rootElement.set({
+		element: newDataTypeTemplatesElement
+	})
+}
+
+function createPrivateElement() {
+	const currentRootElement = get(rootElement)?.element
+	if (!currentRootElement)
+		return console.error('No Root DataTypeTemplates element found')
+
+	const newPrivateElement = pluginStore.addElementToXmlDocument({
+		parentElement: currentRootElement,
+		newElementTagName: 'Private',
+		elementAttributes: {
+			type: 'type-designer-plugin'
+		}
+	})
+
+	privateElement.set({
+		element: newPrivateElement,
+		type: 'type-designer-plugin'
+	})
+}
+
+function createPluginXmlStructure() {
+	if (get(isStructureCreated)) return
+
+	if (!get(rootElement)) createDataTypeTemplateElement()
+	if (!get(privateElement)) createPrivateElement()
+
+	isStructureCreated.set(true)
+}
+
+//==== PUBLIC ACTIONS
+
+function addNewType(columnKey: keyof typeof SCD_ELEMENTS) {
+	createPluginXmlStructure()
+
+	const currentPrivateElement = get(privateElement)?.element
+	if (!currentPrivateElement)
+		return console.error('No Private element found in DataTypeTemplates')
+
+	const iterateType = get(typeElements)[`${columnKey}Types`].length + 1
+
+	const newElementId = `${SCD_ELEMENTS[columnKey].type.baseId}${iterateType}`
+	const newElementName = `${SCD_ELEMENTS[columnKey].type.baseName}${iterateType}`
+
+	const insertElementBefore = SCD_ELEMENTS[columnKey].type.insertBefore
+	const firstElementOfNextType = insertElementBefore
+		? get(typeElements)[insertElementBefore][0]?.element
+		: undefined
+
+	const newElement = pluginStore.addElementToXmlDocument({
+		parentElement: currentPrivateElement,
+		newElementTagName: SCD_ELEMENTS[columnKey].type.tag,
+		namespace: 'td',
+		elementAttributes: {
+			id: newElementId,
+			name: newElementName
+		},
+		insertBefore: firstElementOfNextType
+	})
+}
+
+//==== STORE INITIALIZATION
+
+function init(newXmlDocument: XMLDocument | undefined) {
 	if (!newXmlDocument) return
-	setRootElement(newXmlDocument)
-	setSubElements(newXmlDocument)
+
+	setDataTypesTemplateService(newXmlDocument)
+
+	setRootElement()
+	setPrivateElement()
+	setTypeElements()
 }
 
+//==== PUBLIC STORE
 export const dataTypeTemplatesStore = {
 	//state
 	dataTypeTemplatesRootElement: rootElement,
-	dataTypeTemplatesSubElements: subElements,
+	dataTypeTemplatesSubElements: typeElements,
 	//actions
-	init
+	init,
+	addNewType
 }
--- packages/plugins/type-designer/src/stores/plugin.store.ts
+++ packages/plugins/type-designer/src/stores/plugin.store.ts
@@ -1,17 +1,72 @@
 // SVELTE
-import { writable } from 'svelte/store'
+import { writable, get } from 'svelte/store'
+// CORE
+import { createElementWithNS, newActionEvent } from '@oscd-plugins/core'
+// TYPES
+import type { DataTypeTemplates } from '@oscd-plugins/core'
 
 //====== STATE ======//
 const xmlDocument = writable<XMLDocument | undefined>(undefined)
 const pluginHostElement = writable<Element>()
 
+//====== ACTIONS ======//
+
+function createAndDispatchActionEvent(
+	parent: Element,
+	element: Element,
+	reference: Element | undefined = undefined
+) {
+	const event = newActionEvent({
+		new: {
+			parent,
+			element,
+			reference
+		}
+	})
+
+	get(pluginHostElement).dispatchEvent(event)
+}
+
+function addElementToXmlDocument({
+	parentElement,
+	namespace,
+	newElementTagName,
+	elementAttributes,
+	insertBefore
+}: {
+	parentElement: Element
+	namespace?: string
+	newElementTagName: DataTypeTemplates.AllowedTags
+	elementAttributes?: Record<string, string | null>
+	insertBefore?: Element
+}) {
+	const currentXmlDocument = get(xmlDocument)
+	if (!currentXmlDocument) throw new Error('Xml document is not set')
+
+	const payload = {
+		xmlDocument: currentXmlDocument,
+		tag: newElementTagName,
+		...(elementAttributes && { attributes: elementAttributes }),
+		...(namespace && { namespace })
+	}
+
+	const newElement = createElementWithNS(payload)
+
+	createAndDispatchActionEvent(parentElement, newElement, insertBefore)
+
+	return newElement
+}
+
 //====== INITIALIZATION ======//
 
-function init({
-	newXMLDocument,
+async function init({
+	newXmlDocument,
 	newPluginHostElement
-}: { newXMLDocument: XMLDocument | undefined; newPluginHostElement: Element }) {
-	xmlDocument.set(newXMLDocument)
+}: {
+	newXmlDocument: XMLDocument | undefined
+	newPluginHostElement: Element
+}) {
+	xmlDocument.set(newXmlDocument)
 	pluginHostElement.set(newPluginHostElement)
 }
 
@@ -19,6 +74,7 @@ export const pluginStore = {
 	//state
 	xmlDocument,
 	pluginHostElement,
-	//initialization
+	//actions
+	addElementToXmlDocument,
 	init
 }
