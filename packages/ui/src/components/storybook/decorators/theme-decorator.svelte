7ccf194509250d2f334066098c21567c692c1efd
Michel Guerin <100696308+michelguerin@users.noreply.github.com>
2024-10-24 14:13:28 +0200

    fix(ui): add workaround component to fix the hover issue on IconButton (#150)
    * fix(ui): add workaround component to fix the hover issue on IconButton
    
    * feat(ui): remove unecessary comments

---
+++ packages/ui/src/components/storybook/decorators/theme-decorator.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+import Theme from '@/components/theme/theme.svelte'
+</script>
+
+<Theme>
+	<slot />
+</Theme>
--- packages/ui/src/components/theme/theme.svelte
+++ packages/ui/src/components/theme/theme.svelte
@@ -3,10 +3,10 @@
 <link rel="stylesheet" href={themeCss} />
 <link rel="stylesheet" href={svelteMaterialUiCss} />
 
-{#if isStandAloneMode}
+{#if mode.isStandAlone}
 	<DevMenuBar pluginType={pluginType}/>
-	{/if}
-<tscd-theme>
+{/if}
+<tscd-theme style={cssDynamicStyles}>
 	<slot />
 </tscd-theme>
 
@@ -18,6 +18,8 @@ import themeUrl from '../../assets/styles/theme.css?url'
 import svelteMaterialUiUrl from 'svelte-material-ui/bare.css?url'
 // DEV COMPONENTS
 import { DevMenuBar } from '../../components/dev'
+// UTILS
+import { setInlineStyles } from '../../utils'
 // TYPES
 import type { PluginType } from '@oscd-plugins/core'
 
@@ -26,15 +28,69 @@ import type { PluginType } from '@oscd-plugins/core'
 // props
 export let pluginType: PluginType | undefined = undefined
 
+// local variables
+
 // styles
 const baseURL = new URL(import.meta.url)
 const fontCss = new URL(fontsUrl, baseURL).href
 const themeCss = new URL(themeUrl, baseURL).href
 const svelteMaterialUiCss = new URL(svelteMaterialUiUrl, baseURL).href
 
+const cssFixedStyles = {
+	'header-height': '',
+	'global-height': '',
+	'global-background-color': ''
+}
+
 //====== REACTIVITY ======//
+
 // stand alone mode works with this setting vite --mode STAND_ALONE
-$: isStandAloneMode =
-	import.meta.env.DEV && import.meta.env.MODE === 'STAND_ALONE'
-$: if (isStandAloneMode) import('normalize.css/normalize.css')
+$: mode = {
+	isStandAlone: import.meta.env.DEV && import.meta.env.MODE === 'STAND_ALONE',
+	isStorybook: import.meta.env.DEV && import.meta.env.MODE === 'STORYBOOK'
+}
+$: if (mode.isStandAlone) import('normalize.css/normalize.css')
+// style
+$: cssFixedStyles['header-height'] = setHeaderHeight(mode)
+$: cssFixedStyles['global-height'] = setGlobalHeight(mode)
+$: cssFixedStyles['global-background-color'] = setGlobalBackgroundColor(mode)
+$: cssDynamicStyles = setInlineStyles(cssFixedStyles)
+
+//====== FUNCTIONS ======//
+
+function setGlobalHeight(mode: {
+	isStandAlone: boolean
+	isStorybook: boolean
+}) {
+	if (mode.isStorybook) return 'inherit'
+	return 'calc(100vh - var(--header-height))'
+}
+
+function setHeaderHeight(mode: {
+	isStandAlone: boolean
+	isStorybook: boolean
+}) {
+	if (mode.isStorybook) return '0px'
+	if (mode.isStandAlone) return '64px'
+	return '112px'
+}
+
+function setGlobalBackgroundColor(mode: {
+	isStandAlone: boolean
+	isStorybook: boolean
+}) {
+	if (mode.isStorybook) return 'inherit'
+	return 'var(--mdc-theme-surface)'
+}
 </script>
+
+<style>
+	tscd-theme {
+		font-family: "Roboto", sans-serif;
+		height: var(--global-height);
+		display: flex;
+		align-items: stretch;
+		position: relative;
+		background-color: var(--global-background-color);
+	}
+</style>
