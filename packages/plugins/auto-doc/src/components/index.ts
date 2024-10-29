74d062288ebae4c1057663fc5ae65f0b4821e1d9
Sergio Alvarenga <s.alvarenga@sprinteins.com>
2024-10-29 16:55:09 +0100

    create truncate component

--- packages/plugins/auto-doc/src/components/index.ts
+++ packages/plugins/auto-doc/src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as ElementsTypeContainer } from './doc-template-container/doc-template-container.svelte';
 export { default as Table } from './data-table/data-table.svelte';
 export { default as Tooltip } from './tooltip/tooltip.svelte';
+export { default as Truncate } from './truncate/truncate.svelte';
