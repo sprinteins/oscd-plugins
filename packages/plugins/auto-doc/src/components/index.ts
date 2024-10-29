3e29452ba8383203b3cc9a8057ea28e4f9f1282f
Sergio Alvarenga <s.alvarenga@sprinteins.com>
2024-10-29 12:01:52 +0100

    create tooltip component

--- packages/plugins/auto-doc/src/components/index.ts
+++ packages/plugins/auto-doc/src/components/index.ts
@@ -1,2 +1,3 @@
 export { default as ElementsTypeContainer } from './doc-template-container/doc-template-container.svelte';
 export { default as Table } from './data-table/data-table.svelte';
+export { default as Tooltip } from './tooltip/tooltip.svelte';
---
+++ packages/plugins/auto-doc/src/components/tooltip/tooltip.svelte
@@ -0,0 +1,92 @@
+<script lang="ts">
+    import { onDestroy } from 'svelte';
+  
+    export let text = ''; 
+    export let position = 'top'; 
+    export let delayInMS = 500; 
+  
+    let isTooltipVisible = false;
+    let timeoutId: number;
+  
+    const showTooltip = () => {
+      timeoutId = setTimeout(() => {
+        isTooltipVisible = true;
+      }, delayInMS);
+    };
+  
+    const hideTooltip = () => {
+      clearTimeout(timeoutId);
+      isTooltipVisible = false;
+    };
+  
+    onDestroy(() => {
+      clearTimeout(timeoutId);
+    });
+  </script>
+
+  <div
+        class="tooltip-container"
+        on:mouseenter={showTooltip}
+        on:mouseleave={hideTooltip}
+    >
+        <slot></slot>
+
+        {#if isTooltipVisible}
+            <div class="tooltip {position} visible">
+                {text}
+            </div>
+        {/if}
+  </div>
+  
+  <style>
+    .tooltip-container {
+      position: relative;
+      display: inline-block;
+    }
+  
+    .tooltip {
+      position: absolute;
+      padding: 0.5em 1em;
+      background-color: #333;
+      color: white;
+      font-size: 0.875rem;
+      border-radius: 4px;
+      white-space: nowrap;
+      z-index: 10;
+      opacity: 0;
+      transform: translateY(10px);
+      transition: opacity 0.2s ease, transform 0.2s ease;
+    }
+  
+    .tooltip.top {
+      bottom: 100%;
+      left: 50%;
+      transform: translate(-50%, -10px);
+    }
+  
+    .tooltip.bottom {
+      top: 100%;
+      left: 50%;
+      transform: translate(-50%, 10px);
+    }
+  
+    .tooltip.left {
+      right: 100%;
+      top: 50%;
+      transform: translate(-10px, -50%);
+    }
+  
+    .tooltip.right {
+      left: 100%;
+      top: 50%;
+      transform: translate(10px, -50%);
+    }
+  
+    .tooltip.visible {
+      opacity: 1;
+      transform: translateY(0);
+    }
+  </style>
+  
+
+  
