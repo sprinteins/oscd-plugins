b2ae9118af0196411bb6c01f7db881e655aab349
Sergio Alvarenga <s.alvarenga@sprinteins.com>
2024-11-04 17:47:24 +0100

    type the routes object

--- packages/plugins/auto-doc/src/routes/routes.ts
+++ packages/plugins/auto-doc/src/routes/routes.ts
@@ -1,7 +1,15 @@
 import {TemplateOverview} from "@/pages"
 import {wrap} from 'svelte-spa-router/wrap'
+import type { SvelteComponent, ComponentType } from 'svelte'
+import type { WrappedComponent } from "svelte-spa-router"
 
-export const routes = {
+type Page = ComponentType<SvelteComponent> | WrappedComponent
+
+type Route = {
+    [key: string]: Page
+}
+
+export const routes : Route = {
 	"/" : TemplateOverview,
 	"/create" : wrap({
 		asyncComponent: ()=> import("@/pages/template-creation/template-creation.svelte")
