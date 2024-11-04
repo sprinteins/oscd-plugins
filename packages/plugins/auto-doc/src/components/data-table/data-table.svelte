64c415ea0ebdac57833281809b5601fc9d3a31b5
Sergio Alvarenga <71974638+salvar3nga@users.noreply.github.com>
2024-11-04 15:50:05 +0100

    [FEATURE]:  Auto Doc | Template overview (#148)
    * feat(core): rename of API SCD core function with the version number
    
    + add open scd event
    
    * chore(type-designer): move the plugin from uilib to a dedicated package inside plugins
    
    * feat(ui): create the ui package
    
    * feat(ui): add mui theme component
    
    * feat(ui): add dev bar component
    
    * feat(ui): add storybook
    
    * feat(monorepo): add plugin abstraction to core
    
    * feat(monorepo): add launch command
    
    * feat(core): handle documentation create event
    
    * feat(plugin): prepare plugin abstraction to standardized create-plugin package
    
    * feat(core): renaming edition handler in plugin dev instance
    
    * setup the base for the plugin development
    
    * rename folder and file
    
    * fix compile errors
    
    * display option to upload and save scd file
    
    * create Table component for the overview
    
    * add counter for total items selected
    
    * create date formating Fn
    
    * set table bg color
    
    * add new icons to the table
    
    * place table on the center
    
    * add border radius to table
    
    * add back the missing icons
    
    * create templatesOverview page
    
    * move template data from table to the tamplate-overview page
    
    * remove unused styles
    
    * implement review Feedback
    
    * replace btn with smui/button
    
    * hook secondary theme color to btn
    
    * rename export document to generate document
    
    * create tooltip component
    
    * use tooltip on table action icons
    
    * exchange beta dependencies with up to date one
    
    * create truncate component
    
    * create tooltip position type
    
    * align indentation
    
    * fix type of timeoutId
    
    * truncate template title
    
    * fix app crashing error
    
    ---------
    
    Co-authored-by: Michel Guerin <dev@guerin.at>
    Co-authored-by: Felix Wagner <f.wagner@sprinteins.com>
    Co-authored-by: Sergio Alvarenga <s.alvarenga@sprinteins.com>

---
+++ packages/plugins/auto-doc/src/components/data-table/data-table.svelte
@@ -0,0 +1,126 @@
+<script lang="ts">
+    import DataTable, { Head, Body, Row, Cell } from '@smui/data-table';
+    import Checkbox from '@smui/checkbox';
+    import {Tooltip, Truncate} from "@/components"
+    import type {Template} from "@/pages/template-overview/types.template-overview";
+    import {CustomIconButton} from "@oscd-plugins/ui/src/components"
+
+
+
+    export let allTemplates : Template[];
+
+    let selectedTemplates : Template[] = [];
+
+0
+    function isInvalidDate(date: Date){
+        return isNaN(date.getTime());
+    }
+
+    function formatDate(date: Date, defaultString = "-"): string{
+        if(isInvalidDate(date)){ return defaultString;}
+
+        return `${getDD_MM_YYYYFromDate(date)}, ${getHH_MM_FromDate(date)}`;
+    }
+
+    function getHH_MM_FromDate(date: Date,): string{
+
+        const timeOptions: Intl.DateTimeFormatOptions = { 
+            hour: "numeric",
+            minute: "2-digit"
+        };
+
+        const extractedHH_MM = new Intl.DateTimeFormat("en-US", timeOptions).format(date);
+        return extractedHH_MM;
+    }
+    function getDD_MM_YYYYFromDate(date: Date): string{
+        const dateOptions: Intl.DateTimeFormatOptions = { 
+            month:   "2-digit" as const, 
+            day:     "2-digit" as const,
+            year:    "numeric" as const,
+        };
+
+        const DD_MM_YYYY = new Date(date).toLocaleDateString("de-DE", dateOptions); 
+        return DD_MM_YYYY;
+    }
+
+</script>
+
+
+
+<div class="table-container">      
+    <DataTable>
+        <Head>
+            <Row>
+            <Cell checkbox>
+                <Checkbox />
+            </Cell>
+            <Cell class="big">Name</Cell>
+            <Cell>Last Edited</Cell>
+            <Cell>Description</Cell>
+            <Cell></Cell>
+            </Row>
+        </Head>
+        <Body>
+            {#each allTemplates as template (template.name)}
+                <Row>
+                    <Cell checkbox>
+                        <Checkbox
+                            bind:group={selectedTemplates}
+                            value={template}
+                            valueKey={template.name}
+                        />
+                    </Cell>
+                    <Cell>
+                        <Truncate text={template.name} maxChars={40} tooltipPosition="top"/>
+                    </Cell>
+                    <Cell>{formatDate(template.lastEdited)}</Cell>
+                    <Cell>
+                        <Truncate text={template.description} maxChars={120} tooltipPosition="top"/>
+                    </Cell>
+                    <Cell>
+                    <div class="action-btns">
+                        <Tooltip text="Edit">
+                            <CustomIconButton icon="edit" color="black"/>
+                        </Tooltip>
+                        <Tooltip text="Delete">
+                            <CustomIconButton icon="delete" color="black"/>
+                        </Tooltip>
+                        <Tooltip text="Duplicate">
+                            <CustomIconButton icon="content_copy" color="black"/>
+                        </Tooltip>
+                        <Tooltip text="Download" position="left">
+                            <CustomIconButton icon="download" color="black"/>
+                        </Tooltip>
+                    </div>
+                    </Cell>
+                </Row>
+            {/each}
+        </Body>
+    </DataTable>
+</div>
+
+<style lang="scss">
+    .table-container{
+       width: 100%;
+       display: flex;
+       flex-direction: column;
+       align-items: center;
+    }
+    .table-container{
+
+        & :global(.mdc-data-table__table-container),
+        :global(.mdc-data-table__header-cell)
+        {
+            background-color: rgba(255,255,255);
+        }
+
+        & :global(.mdc-data-table__table-container),
+         :global(.mdc-data-table){
+            border-radius: 10px;
+        }
+
+        & :global(.mdc-data-table__cell){
+            overflow-y: visible;
+        }
+    }
+</style>
