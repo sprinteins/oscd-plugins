e34f34873ded3cd27acff97ed9afecffecf75b26
Illia Solovei <i.solovei@sprinteins.com>
2024-11-05 11:21:28 +0100

    feat: restart the feature on a new base

--- packages/plugins/type-designer/src/views/elements-type-container.svelte
+++ packages/plugins/type-designer/src/views/elements-type-container.svelte
@@ -12,55 +12,133 @@ import ContentCard from '@/components/content-card.svelte'
 import { elementsTypesStore } from '@/stores/elements-types.store'
 import { dataTypeTemplatesStore } from '@/stores/data-types-templates.store'
 // TYPES
-import type { Entries } from '@oscd-plugins/core'
+import type { DataTypeTemplates, Entries } from '@oscd-plugins/core'
+import { dndzone } from 'svelte-dnd-action'
 
 //====== INITIALIZATION ======//
 const { columns } = elementsTypesStore
 
 $: columnsEntries = Object.entries($columns) as Entries<typeof $columns>
+
+let dragState: {
+	sourceColumn?: string
+	targetColumn?: string
+	items?: DataTypeTemplates.TypeElement[]
+} = {}
+
+interface DndConsiderEvent<T> {
+	items: T[]
+	info: {
+		trigger: 'pointer' | 'keyboard'
+		id?: string
+		index?: number
+	}
+}
+
+interface DndFinalizeEvent<T> {
+	items: T[]
+	info: {
+		trigger: 'pointer' | 'keyboard'
+		id?: string
+		index?: number
+	}
+}
+
+function handleConsiderDragEvent(
+	columnKey: string,
+	event: CustomEvent<DndConsiderEvent<DataTypeTemplates.TypeElement>>
+) {
+	dragState.targetColumn = columnKey
+	dragState.items = event.detail.items
+
+	if (!dragState.sourceColumn) {
+		dragState.sourceColumn = columnKey
+	}
+	elementsTypesStore.updateColumnTypes(columnKey, event.detail.items)
+}
+
+function handleFinalizeDragEvent(
+	columnKey: string,
+	event: CustomEvent<DndFinalizeEvent<DataTypeTemplates.TypeElement>>
+) {
+	if (dragState.sourceColumn && dragState.targetColumn) {
+		if (dragState.sourceColumn !== dragState.targetColumn) {
+			// TODO ts error
+			const sourceItems = $columns[dragState.sourceColumn].types.filter(
+				(item) => !event.detail.items.includes(item)
+			)
+			// TODO no interface to update
+			elementsTypesStore.updateColumnTypes(
+				dragState.sourceColumn,
+				sourceItems
+			)
+			elementsTypesStore.updateColumnTypes(
+				dragState.targetColumn,
+				event.detail.items
+			)
+		}
+	}
+
+	// Reset drag state
+	dragState = {}
+}
 </script>
 
 <div class="columns-container" id="type-designer-columns">
-  {#if $columns}
-    {#each columnsEntries as [ key, column ]}
-      <Paper class="column-container {`column-container--${column.visible ? "expanded" : "collapsed"}`}">
-        <article class="column">
-          <div class="column-header">
-            {#if column.visible}
-              <h1>{column.name}</h1>
-            {:else}
-              <h1>{column.name} (hidden)</h1>
-            {/if}
-						<CustomIconButton
-							class="visibility-button"
-							icon={column.visible ? "visibility" : "visibility_off"}
-							on:click={() => elementsTypesStore.toggleColumnVisibility(key)}
-						/>
-          </div>
-          {#if column.visible}
-            <Content class="content">
-              <div class="element-types">
-                {#each column.types as typeElement, index}
-                  <ContentCard name={ typeElement.name || SCD_ELEMENTS[key].type.baseName + ( index +1) }  currentColumn={key} {typeElement} />
-                {/each}
-              </div>
-            </Content>
-          {/if}
-					{#if column.visible && column.name !== SCD_ELEMENTS.lNode.element.name}
-						<div class="add-button-container">
-						<Button
-								class="add-button"
-								on:click={() => dataTypeTemplatesStore.addNewType(key)}
-							>
-								New {column.name}
-							</Button>
-						</div>
-					{/if}
-        </article>
-      </Paper>
-    {/each}
-  {/if}
-</div>
+	{#if $columns}
+	  {#each columnsEntries as [key, column]}
+		<Paper class="column-container {`column-container--${column.visible ? "expanded" : "collapsed"}`}">
+		  <article class="column">
+			<div class="column-header">
+			  {#if column.visible}
+				<h1>{column.name}</h1>
+			  {:else}
+				<h1>{column.name} (hidden)</h1>
+			  {/if}
+			  <CustomIconButton
+				class="visibility-button"
+				icon={column.visible ? "visibility" : "visibility_off"}
+				on:click={() => elementsTypesStore.toggleColumnVisibility(key)}
+			  />
+			</div>
+			{#if column.visible}
+			  <Content class="content">
+				<div class="element-types">
+				  <div 
+					use:dndzone={{
+					  items: column.types,
+					  flipDurationMs: 200,
+					  type: 'columns'
+					}}
+					on:consider={handleConsiderDragEvent}
+					on:finalize={handleFinalizeDragEvent}
+				  >
+					{#each column.types as typeElement, index}
+					  <ContentCard 
+						name={typeElement.name || SCD_ELEMENTS[key].type.baseName + (index + 1)}
+						currentColumn={key}
+						{typeElement}
+					  />
+					{/each}
+				  </div>
+				</div>
+			  </Content>
+			{/if}
+			{#if column.visible && column.name !== SCD_ELEMENTS.lNode.element.name}
+			  <div class="add-button-container">
+				<Button
+				  class="add-button"
+				  on:click={() => dataTypeTemplatesStore.addNewType(key)}
+				>
+				  New {column.name}
+				</Button>
+			  </div>
+			{/if}
+		  </article>
+		</Paper>
+	  {/each}
+	{/if}
+  </div>
 
 <style>
 	.columns-container {
