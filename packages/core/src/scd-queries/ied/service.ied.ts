ede49faf2f6f33d8fbf974daf3d4b451051b7960
Michel Guerin <dev@guerin.at>
2024-11-27 11:24:21 +0100

    refactor(uilib/documentation): add conditional rendering on print + remove comments

--- packages/core/src/scd-queries/ied/service.ied.ts
+++ packages/core/src/scd-queries/ied/service.ied.ts
@@ -22,10 +22,6 @@ export class IEDService {
 			return {
 				iedName: ied.name,
 				iedDetails: this.parseDetails(ied.element),
-<<<<<<< HEAD
-=======
-				// published: this.findPublishedMessages(ied),
->>>>>>> 050a526 (feat(core): migrating uc-communication to domain like files (ied))
 				published: this.findPublishedMessages(ied),
 				received: this.findReceivedMessages(ied, ieds)
 			}
