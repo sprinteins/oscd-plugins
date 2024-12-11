92bf24aad905aad86a7975f10fab5b5528e6183d
Felix Wagner <f.wagner@sprinteins.com>
2024-12-11 11:44:40 +0100

    add code to svelte to test signallist

--- packages/plugins/auto-doc/src/components/doc-template-container/just-a-random.svelte
+++ packages/plugins/auto-doc/src/components/doc-template-container/just-a-random.svelte
@@ -1,15 +1,6 @@
 <div class="container" bind:this={currentHtmlContext} >
 	{#if showFields}
 		<div class="input-fields">
-			{#each fieldsConfig[currentFieldsToShow] as field}
-				<label for={field.name}>{field.label}:</label>
-				<input
-					type={field.type}
-					id={field.name}
-					name={field.name}
-					on:input={handleInputChange}
-				/>
-			{/each}
 			<Button on:click={addElement}>
 				Create element
 			</Button>
@@ -24,46 +15,15 @@
 // COMPONENTS
 import Button from '@smui/button'
 // STORES
-import { xmlDocumentStore } from '@/stores'
+import { docTemplatesStore, signallistStore } from '@/stores'
 // CONSTANTS
-import { ELEMENT_NAMES } from '@oscd-plugins/core'
 // TYPES
-import type { AllowedElements, DataTypeTemplates } from '@oscd-plugins/core'
+import type { AllowedElements } from '@oscd-plugins/core'
 
 //==== INITIALIZATION ====//
 
-//constants
-const fieldsConfig: Record<
-	AllowedElements,
-	{ name: string; label: string; type: 'text' }[]
-> = {
-	ied: [
-		{ name: 'id', label: 'ID', type: 'text' },
-		{ name: 'name', label: 'Name', type: 'text' },
-		{ name: 'desc', label: 'Description', type: 'text' },
-		{ name: 'manufacturer', label: 'Manufacturer', type: 'text' },
-		{ name: 'type', label: 'Type', type: 'text' },
-		{ name: 'originalSclRevision', label: 'SclRevision', type: 'text' },
-		{ name: 'originalSclVersion', label: 'SclVersion', type: 'text' },
-		{ name: 'owner', label: 'Owner', type: 'text' }
-	],
-	lDevice: [
-		{ name: 'id', label: 'ID', type: 'text' },
-		{ name: 'desc', label: 'Description', type: 'text' },
-		{ name: 'inst', label: 'Instance', type: 'text' }
-	]
-}
 
-const buttons = [
-	{
-		name: ELEMENT_NAMES.ied,
-		onClick: () => toggleFields('ied')
-	},
-	{
-		name: ELEMENT_NAMES.lDevice,
-		onClick: () => toggleFields('lDevice')
-	}
-]
+
 
 //refs
 let showFields = false
@@ -73,27 +33,26 @@ let currentHtmlContext: HTMLElement
 
 //==== FUNCTIONS ====//
 
-function toggleFields(elementType: string) {
-	showFields = true
-	currentFieldsToShow = elementType
-	currentElementTypeAttributes = {}
-}
+
 
 function handleInputChange(event: Event) {
 	const target = event.target as HTMLInputElement
 	currentElementTypeAttributes[target.name] = target.value
 }
 
+const buttons = [
+	{
+		name: "add doc",
+		onClick: () => addElement()
+	},
+]
+
 function addElement() {
-	const currentElementTypeTag: DataTypeTemplates.AllowedTag = 'SubstationType'
-
-	xmlDocumentStore.addElementToXmlDocument(
-		currentElementTypeTag,
-		currentElementTypeAttributes,
-		currentHtmlContext
-	)
-
-	showFields = false
+	console.log('create signallist')
+	const signallist = signallistStore.getSignallist()
+	console.log('Message Publishers:', signallist.messagePublishers);
+    console.log('Message Subscribers:', signallist.messageSubscribers);
+    console.log('Invalidities Reports:', signallist.invaliditiesReports);
 }
 </script>
 
