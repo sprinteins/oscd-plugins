cc204f810f47689938b535dcc3da4f76fc369fea
Michel Guerin <100696308+michelguerin@users.noreply.github.com>
2025-01-30 08:31:00 +0100

    Feat/core UI svelte add init actions (#263)
    * feat(type-designer): type-designer reboot
    
    * feat(core-api): add standard revision types + split functions between custom and standard
    
    * feat(core-standard): add first revision and adapt the scripts
    
    * feat(core-ui-svelte): add init actions and stores related to standard definitions

---
+++ packages/core/ui-svelte/src/lib/headless/stores/IEC61850/index.ts
@@ -0,0 +1,9 @@
+// STABLE
+export * from './ed2Rev1.svelte.js'
+
+// UNSTABLE
+export * from './IEC61850_90_30.svelte.js'
+
+// TYPES
+export type * from './types.ed2Rev1.d.ts'
+export type * from './types.IEC61850_90_20.d.ts'
