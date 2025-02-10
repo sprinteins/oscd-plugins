1145883cb75c46d1dc1f98163b4e44359ab64d86
Michel Guerin <dev@guerin.at>
2025-02-10 16:10:08 +0100

    feat(type-designer): remove automation on private fields creation in favor of a creation and deletion on need

---
+++ packages/plugins/type-designer/src/headless/stores/type-elements/wrapper.helper.ts
@@ -0,0 +1,119 @@
+// CORE
+import { pluginGlobalStore } from '@oscd-plugins/core-ui-svelte'
+import {
+	createStandardElement,
+	createCustomElement,
+	createAndDispatchEditEvent
+} from '@oscd-plugins/core-api/plugin/v1'
+// STORE
+import { pluginLocalStore } from '@/headless/stores/index.js'
+
+/**
+ * Creates a new root private wrapper element and dispatch an edit event to create it in the XML document.
+ *
+ * @throws If the `host`, `xmlDocument`, or `rootElement` properties are not available in the respective stores.
+ *
+ * @returns The newly created private wrapper element.
+ */
+export function createCurrentUnstableRevisionRootPrivateWrapper() {
+	if (!pluginGlobalStore.host) throw new Error('No host')
+	if (!pluginGlobalStore.xmlDocument) throw new Error('No XML document')
+	if (!pluginLocalStore.rootElement) throw new Error('No root element')
+
+	const newPrivateWrapper = createStandardElement({
+		xmlDocument: pluginGlobalStore.xmlDocument,
+		currentEdition: pluginLocalStore.currentEdition,
+		currentUnstableRevision: pluginLocalStore.currentUnstableRevision,
+		element: {
+			family: 'private'
+		},
+		attributes: {
+			type: pluginGlobalStore.revisionsStores[
+				pluginLocalStore.currentUnstableRevision
+			].currentNamespacePrefix
+		}
+	})
+
+	createAndDispatchEditEvent({
+		host: pluginGlobalStore.host,
+		edit: {
+			node: newPrivateWrapper,
+			parent: pluginLocalStore.rootElement,
+			reference:
+				pluginLocalStore.rootSubElements?.dataTypeTemplates || null
+		}
+	})
+
+	return newPrivateWrapper
+}
+
+/**
+ * Deletes the root private wrapper by dispatching an edit event.
+ *
+ * @throws If `pluginGlobalStore.host` or `pluginLocalStore.currentUnstableRevisionRootPrivateWrapper` is not defined.
+ */
+export function deleteCurrentUnstableRevisionRootPrivateWrapper() {
+	if (!pluginGlobalStore.host) throw new Error('No host')
+	if (!pluginLocalStore.currentUnstableRevisionRootPrivateWrapper)
+		throw new Error('No private wrapper')
+
+	createAndDispatchEditEvent({
+		host: pluginGlobalStore.host,
+		edit: {
+			node: pluginLocalStore.currentUnstableRevisionRootPrivateWrapper
+		}
+	})
+}
+
+/**
+ * Creates a new EquipmentTypeTemplates element and dispatches an edit event to create it in the XML document.
+ *
+ * @throws If the host, XML document, or root element is not available.
+ *
+ * @returns The first child element of the newly created EquipmentTypeTemplates element, or null if there are no children.
+ */
+export function createEquipmentTypeTemplates() {
+	if (!pluginGlobalStore.host) throw new Error('No host')
+	if (!pluginGlobalStore.xmlDocument) throw new Error('No XML document')
+	if (!pluginLocalStore.rootElement) throw new Error('No root element')
+
+	const newEquipmentTypeTemplatesWrapper = createCustomElement({
+		xmlDocument: pluginGlobalStore.xmlDocument,
+		tagName: 'EquipmentTypeTemplates',
+		namespace: {
+			uri: pluginLocalStore.pluginNamespaceUri,
+			prefix: pluginLocalStore.pluginNamespacePrefix
+		},
+		wrapWithPrivateElement: true
+	})
+
+	createAndDispatchEditEvent({
+		host: pluginGlobalStore.host,
+		edit: {
+			node: newEquipmentTypeTemplatesWrapper,
+			parent: pluginLocalStore.rootElement,
+			reference:
+				pluginLocalStore.rootSubElements?.dataTypeTemplates || null
+		}
+	})
+
+	return newEquipmentTypeTemplatesWrapper.firstElementChild
+}
+
+/**
+ * Deletes the equipment type templates by dispatching an edit event.
+ *
+ * @throws If `pluginGlobalStore.host` or `pluginLocalStore.rootSubElements?.equipmentTypeTemplates` is undefined.
+ */
+export function deleteEquipmentTypeTemplates() {
+	if (!pluginGlobalStore.host) throw new Error('No host')
+	if (!pluginLocalStore.rootSubElements?.equipmentTypeTemplates)
+		throw new Error('No EquipmentTypeTemplates element')
+
+	createAndDispatchEditEvent({
+		host: pluginGlobalStore.host,
+		edit: {
+			node: pluginLocalStore.rootSubElements?.equipmentTypeTemplates
+		}
+	})
+}
