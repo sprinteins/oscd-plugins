a6d72027d0cc0e8344f905389c47bb5957acc3ed
Michel Guerin <dev@guerin.at>
2025-02-10 16:02:19 +0100

    chore(core-api): removing unused types

--- packages/core/api/src/plugin/v1/xml/types.d.ts
+++ packages/core/api/src/plugin/v1/xml/types.d.ts
@@ -21,31 +21,4 @@ export namespace Xml {
 	export type SclCustomElement<TagName> = Element & {
 		tagName: TagName
 	}
-
-	export type SubElementsToSclElements<
-		GenericElement extends IEC61850.AvailableElement<
-			GenericEdition,
-			GenericUnstableRevision
-		>,
-		GenericEdition extends IEC61850.AvailableEdition,
-		GenericUnstableRevision extends
-			| IEC61850.AvailableUnstableRevision<GenericEdition>
-			| undefined = undefined
-	> = {
-		[key in IEC61850.CurrentElementAvailableSubElementsKey<
-			GenericElement,
-			GenericEdition,
-			GenericUnstableRevision
-		>]: IEC61850.CurrentElementSubElements<
-			GenericElement,
-			GenericEdition,
-			GenericUnstableRevision
-		>[key]['array'] extends boolean
-			? Array<SclElement<GenericVersion, key>>
-			: SclElement<GenericVersion, key>
-	}
-
-	export type CommonOptions = {
-		root?: Element
-	}
 }
