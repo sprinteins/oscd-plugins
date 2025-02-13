e6ae9329c031f7ba14e2b2bb8e196134730e6744
pwinkelholzS1 <157472514+pwinkelholzS1@users.noreply.github.com>
2025-02-13 15:22:11 +0100

    Feat/baycontainers (#288)
    * adding basic label display to sidebar
    
    * adding rough baycontainers
    
    * implementing workaround for local positions returned by elk (fixes connections)
    adding top padding to prevent baycontainer label overlapping with IEDs
    increasing baycontainer border size
    
    * removing old port
    
    (cherry picked from commit 8347ad2da7c8962bfdc3308d654cea25d666a16c)
    
    * adding missing config
    
    (cherry picked from commit ce314469b6efa0d898ec595d608a49dce3386198)
    
    * adding background color to baycontainers
    
    * adjusting comments

--- packages/uilib/src/lib/components/diagram/bay-container/bay-container.svelte
+++ packages/uilib/src/lib/components/diagram/bay-container/bay-container.svelte
@@ -12,13 +12,13 @@
 		<div>
 			{node.label}
 		</div>
-		<div class="ied-container">
-			{#each node.children as iedNode}
-				<div class="container" style={`--width:${iedNode.width}px; --height:${iedNode.height}px;`}>
-					<IEDElement node={iedNode}/>
-				</div>
-			{/each}
-		</div>
+		{#each node.children as child}
+			<IEDElement
+				node={child}
+				isSelected={false}
+				testid={`ied-${node.label}`}
+			/>
+		{/each}
 	</div>
 {/if}
 
@@ -27,20 +27,9 @@
 		width: 100%;
 		height: 100%;
 		padding: 0.4rem;
-		border: 1px dashed var(--color-black);
+		border: 2px dashed var(--color-black);
 		border-radius: 5px;
-	}
-
-	.ied-container {
-		display: flex;
-		align-items: center;
-		justify-content: space-around;
-		width: 100%;
-		height: 100%;
-	}
-
-	.container {
-		width: var(--width,200px);
-		height: var(--height,30px);
+		box-sizing: border-box;
+		background-color: rgba(0, 0, 0, 0.1);
 	}
 </style>
