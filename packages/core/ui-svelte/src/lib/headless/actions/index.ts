4e1ded4210e2d761442628a894170b6000ef62b3
Michel Guerin <dev@guerin.at>
2025-02-25 12:54:09 +0100

    feat(core-ui-svelte): add an action to remove warning and issue toast from instance

--- packages/core/ui-svelte/src/lib/headless/actions/index.ts
+++ packages/core/ui-svelte/src/lib/headless/actions/index.ts
@@ -1,3 +1,4 @@
 export * from './useInitPlugin.svelte.js'
 export * from './useInitSsdTemplate.svelte.js'
 export * from './useClickOutside.svelte.js'
+export * from './useRemoveInstanceWarningAndIssueToast.svelte.js'
