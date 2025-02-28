257c6fe899a7e5fe0ad2dbfa75b021f2151bbbd7
Abdelaziz Tina <tinaabdelaziz8@gmail.com>
2025-02-28 16:11:38 +0100

    fix minor typo and styling

--- packages/plugins/io-center/src/ui/components/lp-list/lp-list.svelte
+++ packages/plugins/io-center/src/ui/components/lp-list/lp-list.svelte
@@ -1,40 +1,36 @@
 <script lang="ts">
-    import { LP_TYPE } from "@/headless/constants";
-    import type { LpElement as LpElementType, LpTypes } from "./types.lp-list";
-    import LpElement from "./lp-element.svelte";
-    import SearchBar from "../common/search-bar.svelte";
-    import FilterButtons from "./filter-buttons.svelte";
-    import { Plus } from "lucide-svelte";
-    import CreateLpDialog from "./create-lp-dialog.svelte";
-    import store from "../../../store.svelte";
-    import type { Nullable } from "@/types";
+import { LP_TYPE } from '@/headless/constants'
+import type { Nullable } from '@/types'
+import { Plus } from 'lucide-svelte'
+import store from '../../../store.svelte'
+import SearchBar from '../common/search-bar.svelte'
+import CreateLpDialog from './create-lp-dialog.svelte'
+import FilterButtons from './filter-buttons.svelte'
+import LpElement from './lp-element.svelte'
+import type { LpElement as LpElementType, LpTypes } from './types.lp-list'
 
-    type Props = {
-        addLp: () => void;
-    };
+type Props = {
+	addLp: () => void
+}
 
-    let { addLp }: Props = $props();
+let { addLp }: Props = $props()
 
-    let searchTerm = $state("");
+let searchTerm = $state('')
 
-    let selectedTypeToShow = $state<Nullable<LpTypes>>(null);
-    let showLinked = $state(true);
-    let showUnlinked = $state(true);
+let selectedTypeToShow = $state<Nullable<LpTypes>>(null)
+let showLinked = $state(true)
+let showUnlinked = $state(true)
 
-    const filteredList = $derived.by(() =>
-        store.lpList
-            .filter((item) =>
-                item.name.toLowerCase().includes(searchTerm.toLowerCase()),
-            )
-            .filter((item) =>
-                showLinked && !showUnlinked ? item.isLinked : true,
-            )
-            .filter((item) =>
-                showUnlinked && !showLinked ? !item.isLinked : true,
-            ),
-    );
+const filteredList = $derived.by(() =>
+	store.lpList
+		.filter((item) =>
+			item.name.toLowerCase().includes(searchTerm.toLowerCase())
+		)
+		.filter((item) => (showLinked && !showUnlinked ? item.isLinked : true))
+		.filter((item) => (showUnlinked && !showLinked ? !item.isLinked : true))
+)
 
-    let showDialogue = $state(false);
+let showDialogue = $state(false)
 </script>
 
 <div class="p-6">
@@ -49,7 +45,7 @@
 
     <CreateLpDialog bind:isOpen={showDialogue} {addLp} />
 
-    <SearchBar bind:searchTerm />
+    <SearchBar bind:searchTerm placeholder="Search LP"/>
 
     <div class="mt-2">
         <FilterButtons
--- packages/plugins/io-center/src/ui/components/object-tree/object-tree.svelte
+++ packages/plugins/io-center/src/ui/components/object-tree/object-tree.svelte
@@ -1,53 +1,53 @@
 <script lang="ts">
-	import { searchTree } from "@/headless/utils";
-	import store from "../../../store.svelte";
-	import TreeNode from "./tree-node.svelte";
-	import SearchBar from "../common/search-bar.svelte";
-	import type { TreeNode as TreeNodeType } from "./types.object-tree";
-	import { NODE_TYPE } from "@/headless/constants";
-	import { canvasStore } from "../canvas/canvas-store.svelte";
-	import {
-		addObjectToCanvas,
-		clearObjectCanvas,
-		removeObjectFromCanvas,
-		toggleObjectInCanvas,
-	} from "@/headless/stores/canvas-operations.svelte";
+import { NODE_TYPE } from '@/headless/constants'
+import {
+	addObjectToCanvas,
+	clearObjectCanvas,
+	removeObjectFromCanvas,
+	toggleObjectInCanvas
+} from '@/headless/stores/canvas-operations.svelte'
+import { searchTree } from '@/headless/utils'
+import store from '../../../store.svelte'
+import { canvasStore } from '../canvas/canvas-store.svelte'
+import SearchBar from '../common/search-bar.svelte'
+import TreeNode from './tree-node.svelte'
+import type { TreeNode as TreeNodeType } from './types.object-tree'
 
-	let searchTerm = $state("");
+let searchTerm = $state('')
 
-	let filteredTree = $derived(searchTree(store.objectTree, searchTerm));
+let filteredTree = $derived(searchTree(store.objectTree, searchTerm))
 
-	function addObjectsRecursievlyToCanvas(treeNode: TreeNodeType) {
-		if (treeNode.children && treeNode.children?.length - 1 > 0) {
-			for (const children of treeNode.children) {
-				addObjectsRecursievlyToCanvas(children);
-			}
-		}
-		if (treeNode.type === NODE_TYPE.dataObjectInstance) {
-			addObjectToCanvas(treeNode);
+function addObjectsRecursievlyToCanvas(treeNode: TreeNodeType) {
+	if (treeNode.children && treeNode.children?.length - 1 > 0) {
+		for (const children of treeNode.children) {
+			addObjectsRecursievlyToCanvas(children)
 		}
 	}
+	if (treeNode.type === NODE_TYPE.dataObjectInstance) {
+		addObjectToCanvas(treeNode)
+	}
+}
 
-	function removeObjectsRecursievlyFromCanvas(treeNode: TreeNodeType) {
-		if (treeNode.children && treeNode.children?.length - 1 > 0) {
-			for (const children of treeNode.children) {
-				removeObjectsRecursievlyFromCanvas(children);
-			}
-		}
-		if (treeNode.type === NODE_TYPE.dataObjectInstance) {
-			removeObjectFromCanvas(treeNode);
+function removeObjectsRecursievlyFromCanvas(treeNode: TreeNodeType) {
+	if (treeNode.children && treeNode.children?.length - 1 > 0) {
+		for (const children of treeNode.children) {
+			removeObjectsRecursievlyFromCanvas(children)
 		}
 	}
-
-	function hasAllChildrenSelected(children: TreeNodeType[]) {
-		return children.every((child) =>
-			canvasStore.dataObjects.some((o) => o.id === child.id),
-		);
+	if (treeNode.type === NODE_TYPE.dataObjectInstance) {
+		removeObjectFromCanvas(treeNode)
 	}
+}
+
+function hasAllChildrenSelected(children: TreeNodeType[]) {
+	return children.every((child) =>
+		canvasStore.dataObjects.some((o) => o.id === child.id)
+	)
+}
 </script>
 
 <div class="p-2">
-	<SearchBar bind:searchTerm />
+	<SearchBar bind:searchTerm placeholder="Search DO" />
 	{#each filteredTree as treeNode (treeNode.id)}
 		<TreeNode
 			{searchTerm}
