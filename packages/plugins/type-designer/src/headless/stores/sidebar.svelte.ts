138d18ab71847bfe4e485915b22cde1bfba81ff2
Michel Guerin <dev@guerin.at>
2025-03-14 13:10:45 +0100

    feat(type-designer): disable sidebar inputs for imported elements details

--- packages/plugins/type-designer/src/headless/stores/sidebar.svelte.ts
+++ packages/plugins/type-designer/src/headless/stores/sidebar.svelte.ts
@@ -7,27 +7,31 @@ class UseSidebarStore {
 	currentElementTypeKey = $state<string | null>(null)
 	currentElementTypeFamily = $state<AvailableTypeFamily | null>(null)
 	currentElementType = $state<TypeElement<AvailableTypeFamily> | null>(null)
-
+	isCurrentElementImported = $state<boolean>(false)
 	//====== ACTIONS ======//
 
 	setCurrentElementType({
 		key,
 		family,
-		element
+		element,
+		isImported
 	}: {
 		key: string
 		family: AvailableTypeFamily
 		element: TypeElement<AvailableTypeFamily>
+		isImported: boolean
 	}) {
 		this.currentElementTypeFamily = family
 		this.currentElementTypeKey = key
 		this.currentElementType = element
+		this.isCurrentElementImported = isImported
 	}
 
 	resetCurrentElementType() {
 		this.currentElementTypeKey = null
 		this.currentElementTypeFamily = null
 		this.currentElementType = null
+		this.isCurrentElementImported = false
 	}
 }
 
