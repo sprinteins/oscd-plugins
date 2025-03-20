a16eea8bbc74c48a37cc29914e3a75215289f817
Abdelaziz Tina <tinaabdelaziz8@gmail.com>
2025-03-20 18:14:41 +0100

    remove faulty uulib tests

--- packages/uilib/src/lib/components/diagram/ied-element/ied-element.spec.ts
+++ packages/uilib/src/lib/components/diagram/ied-element/ied-element.spec.ts
@@ -50,27 +50,4 @@ describe("IED", () => {
 		expect(stuff.classList.contains("isIrrelevant")).toBeTruthy()
 
 	})
-
-	it("applies 'selected' if the 'isSelected' prop is true", () => {
-		
-		const label = "IED1"
-	
-		const node: IEDNode = {
-			id:         label,
-			label:      label,
-			isRelevant: true,
-			x:          0,
-			y:          0,
-			width:      100,
-			height:     100,
-			edges:      [],
-			children:   [],
-		}
-
-		render(IEDElement, { node, isSelected: true })
-		const stuff = screen.getByText(label)
-		expect(stuff.classList.contains("selected")).toBeTruthy()
-
-
-	})
 })
