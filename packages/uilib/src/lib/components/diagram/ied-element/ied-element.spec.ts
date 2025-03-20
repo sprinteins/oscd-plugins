719e6afebc3d985c1d51072220ad304d88a1f354
Abdelaziz Tina <tinaabdelaziz8@gmail.com>
2025-03-20 20:26:39 +0100

    fix test instead of deleting it

--- packages/uilib/src/lib/components/diagram/ied-element/ied-element.spec.ts
+++ packages/uilib/src/lib/components/diagram/ied-element/ied-element.spec.ts
@@ -50,4 +50,25 @@ describe("IED", () => {
 		expect(stuff.classList.contains("isIrrelevant")).toBeTruthy()
 
 	})
+
+	it("applies 'selected' if the 'isSelected' prop is true", () => {
+		
+		const label = "IED1"
+	
+		const node: IEDNode = {
+			id:         label,
+			label:      label,
+			isRelevant: true,
+			x:          0,
+			y:          0,
+			width:      100,
+			height:     100,
+			edges:      [],
+			children:   [],
+		}
+
+		render(IEDElement, { node, isIEDSelected: true })
+		const stuff = screen.getByText(label)
+		expect(stuff.classList.contains("selected")).toBeTruthy()
+	})
 })
