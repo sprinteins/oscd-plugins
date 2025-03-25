04b561d6ead719ffb55171e91beb02e4e9312288
Illia Solovei <i.solovei@sprinteins.com>
2025-03-25 13:30:29 +0100

    feat: visually disable not supported columns

--- packages/plugins/type-designer/src/ui/components/column/column.svelte
+++ packages/plugins/type-designer/src/ui/components/column/column.svelte
@@ -5,7 +5,7 @@ import AddElement from './add-element.svelte'
 import CardCollapsibleWrapper from '../element-card/card-collapsible-wrapper.svelte'
 import { Input } from '@oscd-plugins/core-ui-svelte'
 // STORES
-import { typeElementsStore } from '@/headless/stores'
+import { typeElementsStore, dndStore } from '@/headless/stores'
 // TYPES
 import type {
 	AvailableTypeFamily,
@@ -13,6 +13,7 @@ import type {
 	Column,
 	TypeElementByIds
 } from '@/headless/stores'
+  import { ALLOWED_TARGETS_BY_REF_FAMILY, COLUMN_KEY_TO_TYPE_FAMILY, TYPE_FAMILY } from '@/headless/constants';
 
 //======= INITIALIZATION =======//
 
@@ -35,9 +36,35 @@ const groupedTypeElementsEntries = $derived(
 const capitalizedColumnKey = $derived(
 	columnKey.charAt(0).toUpperCase() + columnKey.slice(1)
 )
+
+const isColumnDisabled = $derived.by(() => {
+    if (!dndStore.isDragging) return false;
+    
+    // Ref
+    if (dndStore.currentSourceRefFamily) {
+        return !ALLOWED_TARGETS_BY_REF_FAMILY[dndStore.currentSourceRefFamily]
+            .some(allowedFamily => {
+                if (Array.isArray(COLUMN_KEY_TO_TYPE_FAMILY[columnKey])) {
+                    return COLUMN_KEY_TO_TYPE_FAMILY[columnKey].includes(allowedFamily);
+                }
+                return COLUMN_KEY_TO_TYPE_FAMILY[columnKey] === allowedFamily;
+            });
+    }
+    
+    // Funktion
+    // if (dndStore.currentSourceTypeFamily === TYPE_FAMILY.function) {
+    //     const allowedFamilies = [TYPE_FAMILY.bay, TYPE_FAMILY.generalEquipment, TYPE_FAMILY.conductingEquipment];
+    //     if (Array.isArray(COLUMN_KEY_TO_TYPE_FAMILY[columnKey])) {
+    //         return !COLUMN_KEY_TO_TYPE_FAMILY[columnKey].some(family => allowedFamilies.includes(family));
+    //     }
+    //     return !allowedFamilies.includes(COLUMN_KEY_TO_TYPE_FAMILY[columnKey] as AvailableTypeFamily);
+    // }
+    
+    return true;
+});
 </script>
 
-<Card.Root class="{columnKey === 'lNodeType' ? 'pb-6' : ''} flex-1 flex flex-col overflow-y-hidden min-h-full " >
+<Card.Root class="{columnKey === 'lNodeType' ? 'pb-6' : ''} flex-1 flex flex-col overflow-y-hidden min-h-full {isColumnDisabled ? 'opacity-40' : ''}" >
 	<Card.Header class="pb-6">
 		<Card.Title>{ column.name}</Card.Title>
 		<Input.Root
