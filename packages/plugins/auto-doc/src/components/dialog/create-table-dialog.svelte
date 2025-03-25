dde281525e38b5d73b611ab49055df93ff16c4a0
Rasul Adasov <rasul@futureblur.com>
2025-03-25 14:31:36 +0100

    feat: limit input and refactor dialog

--- packages/plugins/auto-doc/src/components/dialog/create-table-dialog.svelte
+++ packages/plugins/auto-doc/src/components/dialog/create-table-dialog.svelte
@@ -2,18 +2,26 @@
     import Textfield from "@smui/textfield";
     import Dialog, { Title, Content, Actions } from "@smui/dialog";
     import Button, { Label } from "@smui/button";
-    
+
     export let isOpen = false;
-    export let onHandleSubmit = (rows: number, columns: number) => {};
+    export let onHandleSubmit = (_rows: number, _columns: number) => {};
 
     let rows = 2;
     let columns = 4;
 
-    function handleCancel() {
-        console.log("canceled")
-    }
+    let minRows = 2;
+    let maxRows = 2;
+    let minColumns = 1;
+    let maxColumns = 8;
 
     function handleSubmit() {
+        const isValidInput = Number(rows) && Number(columns);
+        const isWithinBounds = rows >= minRows && rows <= maxRows && columns >= minColumns && columns <= maxColumns;
+        
+        if(!isValidInput || !isWithinBounds) {
+            return;
+        }
+
         onHandleSubmit(rows, columns);
     }
 </script>
@@ -23,25 +31,35 @@
     aria-labeledby="create-table-dialog-title"
     aria-describedby="create-table-dialog-content"
 >
-    <Title id="create-table-dialog-title">Add Table</Title>
+    <Title id="create-table-dialog-title">New Table</Title>
     <Content id="create-table-dialog-content">
-        <Textfield 
-        bind:value={rows}
-        variant="outlined"
-        label={"Rows"}
-        type="number"
-        ></Textfield>
-        <Textfield 
-            bind:value={columns}
-            variant="outlined"
-            label={"Columns"}
-            type="number"
-        ></Textfield>
+        Specify the dimensions. 
+        <div id="create-table-dialog__input">
+            <Textfield 
+                class="mdc-text-field--with-leading-icon"
+                bind:value={rows}
+                variant="outlined"
+                label={"Rows"}
+                type="number"
+                input$min={minRows}
+                input$max={maxRows}
+            >
+            </Textfield>
+            <Textfield 
+                bind:value={columns}
+                variant="outlined"
+                label={"Columns"}
+                type="number"
+                input$min={minColumns}
+                input$max={maxColumns}
+            >
+            </Textfield>
+        </div>
         <Actions>
-            <Button action="none" on:click={handleCancel}>
+            <Button action="none">
                 <Label>Cancel</Label>
             </Button>
-            <Button action="none" on:click={() => handleSubmit()}>
+            <Button action="none" on:click={handleSubmit}>
                 <Label>Add</Label>
             </Button>
         </Actions>
@@ -49,7 +67,14 @@
 </Dialog>
 
 <style>
-    .mdc-dialog__content {
-        padding-top: 10px !important;
-    } 
+    #create-table-dialog__input {
+        display: flex;
+
+        margin-top: 1.5rem;
+        gap: 0.5rem;
+    }
+
+    #create-table-dialog__input :global(label){
+        flex: 1;
+    }
 </style>
