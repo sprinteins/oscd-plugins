cb168617a6cada731786185eb949a4fcac805afa
Michel Guerin <dev@guerin.at>
2025-03-26 16:53:25 +0100

    feat(type-designer): add terminal handler logic in a dedicated component

---
+++ packages/plugins/type-designer/src/ui/components/sidebar/children-options/conducting-equipment.svelte
@@ -0,0 +1,88 @@
+	
+<script lang="ts">
+import { v4 as uuidv4 } from 'uuid'
+// COMPONENTS
+import {
+	Label,
+	pluginGlobalStore,
+	SelectWorkaround
+} from '@oscd-plugins/core-ui-svelte'
+// STORES
+import { sidebarStore } from '@/headless/stores'
+import { createAndDispatchEditEvent } from '@oscd-plugins/core-api/plugin/v1'
+
+//====== FUNCTIONS ======//
+
+const isCurrentSelectDisabled = $derived(
+	sidebarStore.isCurrentElementImported ||
+		(!!sidebarStore.currentElementType?.parameters.childrenOptions
+			.conductingEquipment?.currentValue &&
+			sidebarStore.currentElementType?.parameters.childrenOptions
+				.conductingEquipment?.options.length <= 1)
+)
+
+//====== GETTERS / SETTERS ======//
+
+function getTerminalValue() {
+	return (
+		sidebarStore.currentElementType?.parameters.childrenOptions
+			?.conductingEquipment?.currentValue || 0
+	)
+}
+
+function setTerminalValue(value: number) {
+	if (!sidebarStore.currentElementType) return
+
+	if (
+		sidebarStore.currentElementType.parameters.childrenOptions
+			?.conductingEquipment?.currentValue
+	)
+		sidebarStore.currentElementType.parameters.childrenOptions.conductingEquipment.currentValue =
+			value
+
+	if (!pluginGlobalStore.host) throw new Error('Host not found')
+
+	const currentTerminalsElements =
+		sidebarStore.currentElementType.parameters.childrenOptions
+			.conductingEquipment?.currentTerminalsElements
+
+	if (value === 1 && currentTerminalsElements?.length === 2)
+		createAndDispatchEditEvent({
+			host: pluginGlobalStore.host,
+			edit: {
+				node: currentTerminalsElements[1]
+			}
+		})
+	if (value === 2 && currentTerminalsElements?.length === 1) {
+		const clonedTerminal = currentTerminalsElements[0].cloneNode(
+			false
+		) as Element
+		clonedTerminal.setAttribute('uuid', uuidv4())
+
+		createAndDispatchEditEvent({
+			host: pluginGlobalStore.host,
+			edit: {
+				parent: sidebarStore.currentElementType.element,
+				node: clonedTerminal,
+				reference: null
+			}
+		})
+	}
+}
+</script>
+
+
+{#if sidebarStore.currentElementType?.parameters.childrenOptions.conductingEquipment}
+	<div class="flex justify-between items-center">
+		<Label.Root class="w-4/5">Number of Terminals:</Label.Root>
+		<SelectWorkaround
+			class="w-2/6"
+			disabled={isCurrentSelectDisabled}
+			options={sidebarStore.currentElementType.parameters.childrenOptions.conductingEquipment.options}
+			bind:value={
+				getTerminalValue,
+				setTerminalValue
+			}
+		/>
+	</div>
+{/if}
