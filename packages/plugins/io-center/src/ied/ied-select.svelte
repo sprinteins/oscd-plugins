6ba1c7578a6d2e67b0d6d299c7e4ee259ab97237
Rasul Adasov <rasul@futureblur.com>
2025-03-31 11:34:59 +0200

    Feat: UX Adjustments in IO Center (2) (#413)
    * feat(io-center): make ied-select same width as search field
    
    * fix(io-center): replace dropdown arrow icon

--- packages/plugins/io-center/src/ied/ied-select.svelte
+++ packages/plugins/io-center/src/ied/ied-select.svelte
@@ -21,7 +21,7 @@
 	}
 </script>
 
-<div class="flex-col mx-auto w-4/5 mt-2">
+<div class="flex-col mx-auto w-full px-2 mt-2">
 	<label for="select" class="font-medium">Select an IED</label>
 	<select
 		id="select"
@@ -38,3 +38,15 @@
 		{/each}
 	</select>
 </div>
+
+<style>
+	select {
+		background: url("data:image/svg+xml,<svg height='10px' width='10px' viewBox='0 0 16 16' fill='%23000000' xmlns='http://www.w3.org/2000/svg'><path d='M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z'/></svg>") no-repeat;
+		background-position: calc(100% - 0.75rem) center !important;
+		-moz-appearance:none !important;
+		-webkit-appearance: none !important; 
+		appearance: none !important;
+		padding-right: 2rem !important;
+		background-color: #FAFAFA;
+	}
+</style>
