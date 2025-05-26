54c0c4ea899d761e863e6ab494a7ebfb11c4f0f9
Michel Guerin <100696308+michelguerin@users.noreply.github.com>
2025-05-26 09:16:50 +0200

    Feat/type designer import bay equipments (#468)
    * feat(api): adapt areElementIdentical function with a nea ignoreAttributes parameter
    
    * feat(core-ui): add class prop to tooltip component
    
    * chore(type-designer): remove unused constants
    
    * feat(type-designer): adapt import logic to one import file for all columns
    
    * feat(type-designer): add imports to column and filtering logic
    
    * feat(type-designer): add new import logic to ui components
    
    * chore(type-designer): increment version and update changelog
    
    * chore(type-designer): update changelog

--- packages/plugins/type-designer/src/headless/stores/imports/import-attribute.helper.ts
+++ packages/plugins/type-designer/src/headless/stores/imports/import-attribute.helper.ts
@@ -1,6 +1,10 @@
 import { v4 as uuidv4 } from 'uuid'
+// CORE
+import { ssdStore } from '@oscd-plugins/core-ui-svelte'
 // STORES
 import { pluginLocalStore } from '@/headless/stores'
+// TYPES
+import type { EditEvent, RemoveEvent } from '@/headless/stores'
 
 /**
  * BUSINESS RULES: ~ refers to revision 90-30 page 182
@@ -16,7 +20,6 @@ export function setUuidAttributesRecursively(element: Element) {
 	if (isLNodeType) return
 
 	const uuid = element.getAttribute('uuid')
-	const templateUuid = element.getAttribute('templateUuid')
 	const originUuid = element.getAttributeNS(
 		pluginLocalStore.namespaces.currentUnstableRevision.uri,
 		'originUuid'
@@ -26,10 +29,76 @@ export function setUuidAttributesRecursively(element: Element) {
 		element.setAttributeNS(
 			pluginLocalStore.namespaces.currentUnstableRevision.uri,
 			'originUuid',
-			templateUuid || uuid
+			uuid
 		)
 	element.setAttribute('uuid', uuidv4())
 
 	for (const child of Array.from(element.children))
 		setUuidAttributesRecursively(child)
+}
+
+/**
+ * Updates templateUuid references in imported elements.
+ * This function finds the relationships between originUuid and uuid attributes,
+ * in the imported Document context AND in the working XML Document context.
+ * It then updates any templateUuid references to match the new uuids.
+ *
+ * @param importActions Array of import actions (edit events and optional remove events)
+ * @returns The same array of import actions with updated templateUuid references
+ */
+export function getCurrentImportedActionsWithUpdatedTemplateUuids(
+	importActions: [EditEvent, RemoveEvent | undefined][]
+): [EditEvent, RemoveEvent | undefined][] {
+	let originUuidToUuidMap: Record<string, string> = {}
+
+	for (const [editEvent] of importActions) {
+		originUuidToUuidMap = {
+			...originUuidToUuidMap,
+			...mapElementOriginUuidToUuid(editEvent.node)
+		}
+	}
+
+	const workingXmlDocumentElements =
+		ssdStore.bayTemplateElement?.querySelectorAll('*')
+
+	for (const element of workingXmlDocumentElements || []) {
+		originUuidToUuidMap = {
+			...originUuidToUuidMap,
+			...mapElementOriginUuidToUuid(element)
+		}
+	}
+
+	for (const [editEvent] of importActions)
+		updateTemplateUuidReferencesRecursively(
+			editEvent.node,
+			originUuidToUuidMap
+		)
+
+	return importActions
+}
+
+function mapElementOriginUuidToUuid(element: Element) {
+	const originUuidToUuidMap: Record<string, string> = {}
+
+	const uuid = element.getAttribute('uuid')
+	const originUuid = element.getAttributeNS(
+		pluginLocalStore.namespaces.currentUnstableRevision.uri,
+		'originUuid'
+	)
+
+	if (originUuid && uuid) originUuidToUuidMap[originUuid] = uuid
+
+	return originUuidToUuidMap
+}
+
+function updateTemplateUuidReferencesRecursively(
+	element: Element,
+	originUuidToUuidMap: Record<string, string>
+): void {
+	const templateUuid = element.getAttribute('templateUuid')
+	if (templateUuid && originUuidToUuidMap[templateUuid])
+		element.setAttribute('templateUuid', originUuidToUuidMap[templateUuid])
+
+	for (const child of Array.from(element.children))
+		updateTemplateUuidReferencesRecursively(child, originUuidToUuidMap)
 }
