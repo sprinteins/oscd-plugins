144415cddbf635bb20ffc14c1c222ec299ffdfc7
Christopher Lepski <c.lepski@sprinteins.com>
2025-06-27 15:49:10 +0200

    chore: Add auto doc template for svelte 5

---
+++ packages/plugins/auto-doc2/src/plugin.svelte
@@ -0,0 +1,51 @@
+<svelte:options 
+	customElement={{
+		props: {
+			doc: { reflect: true, type: 'Object'},
+			docName: { reflect: true, type: 'String'},
+			editCount: { reflect: true, type: 'Number'},
+			locale: { reflect: true, type: 'String'},
+			pluginType: { reflect: true, type: 'String'},
+			isCustomInstance: { reflect: true, type: 'Boolean'},
+		}
+	}}
+/>
+
+<main 
+	use:initPlugin={{
+		getDoc: () => doc,
+		getDocName: () => docName,
+		getEditCount: () => editCount,
+		getIsCustomInstance: () => isCustomInstance,
+		getHost: () => $host() || window,
+		theme: 'legacy-oscd-instance',
+		definition: {
+			edition: 'ed2Rev1',
+		}
+	}}
+	data-plugin-name={jsonPackage.name}
+	data-plugin-version={jsonPackage.version}
+>
+	<div class="flex flex-col space-y-9 items-center justify-center h-screen">
+		<h1 class="h1 font-black text-9xl">Hello Plugin 7</h1>
+		<span>See the <i>README</i> file in <b>`packages/template`</b> (oscd-plugins monorepo)</span>
+	</div>
+</main>
+
+
+<script lang="ts">
+// PACKAGE
+import jsonPackage from '../package.json'
+// CORE
+import { initPlugin } from '@oscd-plugins/core-ui-svelte'
+// TYPES
+import type { Utils } from '@oscd-plugins/core-api/plugin/v1'
+
+// props
+const {
+	doc,
+	docName,
+	editCount,
+	isCustomInstance
+}: Utils.PluginCustomComponentsProps = $props()
+</script>
---
+++ packages/plugins/auto-doc2/vite.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { resolve } from 'node:path'
+import dts from 'vite-plugin-dts'
+
+const isDevelopment = process.env.NODE_ENV === 'development'
+
+export default defineConfig({
+	plugins: [svelte(), dts({ rollupTypes: true })],
+	resolve: {
+		alias: {
+			'@': resolve(__dirname, 'src')
+		}
+	},
+	build: {
+		target: 'esnext',
+		lib: {
+			entry: resolve(__dirname, 'src/plugin.ts'),
+			formats: ['es'],
+			fileName: 'index'
+		},
+		sourcemap: isDevelopment ? 'inline' : false
+	},
+	server: {
+		port: 4176,
+		cors: true
+	},
+	preview: {
+		port: 41761,
+		cors: true
+	}
+})
