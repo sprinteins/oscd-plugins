0de2d24a1b45387ce450819818ca7b9f2b4db843
Christopher Lepski <c.lepski@sprinteins.com>
2025-09-05 13:40:49 +0200

    chore: Copy code from NE

---
+++ packages/plugins/network-explorer2/src/external/diagram/ied-element/ied-element.spec.ts
@@ -0,0 +1,74 @@
+import { render, screen } from "@testing-library/svelte"
+import {describe, it, expect} from "vitest"
+
+import { IEDElement } from "."
+import type { IEDNode } from "../nodes"
+
+
+
+describe("IED", () => {
+	it("renders", () => {
+
+		const label = "IED1"
+	
+		const node: IEDNode = {
+			id:         label,
+			label:      label,
+			isRelevant: true,
+			x:          0,
+			y:          0,
+			width:      100,
+			height:     100,
+			edges:      [],
+			children:   [],
+		}
+
+
+		render(IEDElement, { node })
+		const stuff = screen.getByText(label)
+		expect(stuff).toBeTruthy()
+	})
+
+	it("applies 'isIrrelevant' class if the node's 'relevant' prop is false", () => {
+		
+		const label = "IED1"
+	
+		const node: IEDNode = {
+			id:         label,
+			label:      label,
+			isRelevant: false,
+			x:          0,
+			y:          0,
+			width:      100,
+			height:     100,
+			edges:      [],
+			children:   [],
+		}
+
+		render(IEDElement, { node })
+		const stuff = screen.getByText(label)
+		expect(stuff.classList.contains("isIrrelevant")).toBeTruthy()
+
+	})
+
+	it("applies 'selected' if the 'isSelected' prop is true", () => {
+		
+		const label = "IED1"
+	
+		const node: IEDNode = {
+			id:         label,
+			label:      label,
+			isRelevant: true,
+			x:          0,
+			y:          0,
+			width:      100,
+			height:     100,
+			edges:      [],
+			children:   [],
+		}
+
+		render(IEDElement, { node, isIEDSelected: true })
+		const stuff = screen.getByText(label)
+		expect(stuff.classList.contains("selected")).toBeTruthy()
+	})
+})
