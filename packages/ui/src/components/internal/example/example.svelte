e5077b1ccffd32b3c7fb6de02522cd789312081a
andre.hartmann <andre.hartmann@nexmart.com>
2025-09-24 10:08:00 +0200

    chore: migrate internal into ui package

---
+++ packages/ui/src/components/internal/example/example.svelte
@@ -0,0 +1,27 @@
+<script>
+
+    export let name = ""
+</script>
+
+
+<example>
+    <div>
+        <h3>{name}</h3>
+    </div>
+    <slot />
+</example>
+
+
+<style>
+    example {
+        display: block;
+        margin-bottom: 3rem;
+        position: relative;
+        width: 100%;
+        max-height: 100vh;
+        overflow: hidden;
+    }
+    h3 {
+        margin-bottom: 1rem;
+    }
+</style>
