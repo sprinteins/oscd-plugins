74136aeae3465bc8dfcac473e3c1a3f5b5d5d07e
andre.hartmann <andre.hartmann@nexmart.com>
2025-09-24 10:45:14 +0200

    chore: migrate missing icons into ui library

--- packages/ui/src/components/icons/index.ts
+++ packages/ui/src/components/icons/index.ts
@@ -1,2 +1,5 @@
 export { default as IconWrapper } from './icon-wrapper.svelte'
 export { default as IconArrowDropDown } from './icon-arrow-drop-down.svelte'
+export { default as Icons } from "./icons.svelte"
+export * from "./openscd-icons"
+export { default as IconClose } from "./icon-close.svelte"
