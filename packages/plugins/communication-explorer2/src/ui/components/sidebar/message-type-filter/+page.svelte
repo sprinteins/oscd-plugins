ca9496ba0eee9fe39da323054b1dd088e5ba9307
andre.hartmann <andre.hartmann@nexmart.com>
2025-10-13 17:01:44 +0200

    refactor: remove unused storybook examples

--- packages/plugins/communication-explorer2/src/ui/components/sidebar/message-type-filter/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-    import { Example } from "../../../utils/internal/example"
-    import { MessageTypeFilter } from "./"
-</script>
-
-<Example name="Communication Explorer">
-    <MessageTypeFilter filterDisabled={false} selectedMessageTypes={[]} />
-    <!-- <tscd-communication-explorer root={doc} ></tscd-communication-explorer> -->
-</Example>
