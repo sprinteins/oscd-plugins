d2455756543f4f1a8d92a164cb05581cfaef207e
maxschmalenbach <m.schmalenbach@sprinteins.com>
2025-12-03 15:13:53 +0100

    tech(constants) move types to top

--- packages/plugins/auto-doc/src/utils/pdf/constants.ts
+++ packages/plugins/auto-doc/src/utils/pdf/constants.ts
@@ -1,35 +1,33 @@
+export type TextSize = number;
+export type FontStyle = string;
+export type ImageScale = keyof typeof IMAGE_SCALE_FACTORS;
+
 export const TEXT_SIZES = {
   H1: 20,
   H2: 16,
   H3: 14,
   NORMAL: 10,
-}
-
-export type TextSize = number;
-export type FontStyle = string;
-
+};
 
 export const FONT_STYLES = {
-  NORMAL: 'normal',
-  BOLD: 'bold',
-  ITALIC: 'italic',
-}
+  NORMAL: "normal",
+  BOLD: "bold",
+  ITALIC: "italic",
+};
 
 export const IMAGE_SCALE_FACTORS = {
-	small: 0.25,
-	medium: 0.5,
-	large: 1
-} as const
-
-export type ImageScale = keyof typeof IMAGE_SCALE_FACTORS
+  small: 0.25,
+  medium: 0.5,
+  large: 1,
+} as const;
 
 export const PDF_CONSTANTS = {
-	DEFAULT_FONT_SIZE: TEXT_SIZES.NORMAL,
-	INITIAL_PAGE_MARGIN: 10,
-	DEFAULT_LINE_HEIGHT: 7,
-	HORIZONTAL_SPACING: 10,
-	MAX_IMAGE_WIDTH: 186,
-	TEXT_MARGIN_OFFSET: 35,
-	PAGE_BUFFER: 10,
-	NESTED_LIST_INDENT: 10
-} as const
+  DEFAULT_FONT_SIZE: TEXT_SIZES.NORMAL,
+  INITIAL_PAGE_MARGIN: 10,
+  DEFAULT_LINE_HEIGHT: 7,
+  HORIZONTAL_SPACING: 10,
+  MAX_IMAGE_WIDTH: 186,
+  TEXT_MARGIN_OFFSET: 35,
+  PAGE_BUFFER: 10,
+  NESTED_LIST_INDENT: 10,
+} as const;
