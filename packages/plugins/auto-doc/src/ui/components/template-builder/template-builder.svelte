98d156971e50b58a5e3e9d094ba8d0c427f425e4
maxschmalenbach <m.schmalenbach@sprinteins.com>
2025-12-08 15:32:39 +0100

    tech(template-builder): additional padding below to improve ui

--- packages/plugins/auto-doc/src/ui/components/template-builder/template-builder.svelte
+++ packages/plugins/auto-doc/src/ui/components/template-builder/template-builder.svelte
@@ -196,6 +196,7 @@ function handleContentChange(elementId: string, newContent: string) {
 
     .elements-container{
         margin-top: 2rem;
+				margin-bottom: 4rem;
         display: flex;
         gap: 1.5rem;
         flex-wrap: wrap;
