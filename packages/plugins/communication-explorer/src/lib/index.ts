e8d1040d5bc577c3fc822b53f28aa95cb460b8f4
Tobias Menzel <t.menzel@sprinteins.com>
2025-12-09 16:54:31 +0100

    fix: integrate communication explorer into auto-doc and update related configurations

---
+++ packages/plugins/communication-explorer/src/lib/index.ts
@@ -0,0 +1,7 @@
+// Library exports for @oscd-plugins/communication-explorer
+// This module exports components and utilities for use as a Svelte library
+
+export { default as TelemetryView } from '../ui/components/telemetry-view/telemetry-view.svelte'
+
+// Re-export types that consumers might need
+export type { IED } from '@oscd-plugins/core'
