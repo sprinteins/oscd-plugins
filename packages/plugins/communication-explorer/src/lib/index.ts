90b1fa4f6107dc497ebc735fefcf56bb0b501ffb
Tobias Menzel <t.menzel@sprinteins.com>
2025-12-09 17:03:20 +0100

    fix: remove comments

--- packages/plugins/communication-explorer/src/lib/index.ts
+++ packages/plugins/communication-explorer/src/lib/index.ts
@@ -1,7 +1,1 @@
-// Library exports for @oscd-plugins/communication-explorer
-// This module exports components and utilities for use as a Svelte library
-
 export { default as TelemetryView } from '../ui/components/telemetry-view/telemetry-view.svelte'
-
-// Re-export types that consumers might need
-export type { IED } from '@oscd-plugins/core'
