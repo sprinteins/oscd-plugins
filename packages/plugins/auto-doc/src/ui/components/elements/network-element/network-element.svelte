03733c4c6aee228be28a7051cbf281b845a1dea3
maxschmalenbach <m.schmalenbach@sprinteins.com>
2025-12-11 12:05:52 +0100

    revert changes for ne bg bug

--- packages/plugins/auto-doc/src/ui/components/elements/network-element/network-element.svelte
+++ packages/plugins/auto-doc/src/ui/components/elements/network-element/network-element.svelte
@@ -66,12 +66,4 @@ $effect(() => {
 	.network-preview-wrapper :global(*) {
 		pointer-events: none !important;
 	}
-
-	.network-preview-wrapper :global(.svelte-flow) {
-		background-color: transparent !important;
-	}
-
-	.network-preview-wrapper :global(.svelte-flow__background) {
-		background-color: transparent !important;
-	}
 </style>
