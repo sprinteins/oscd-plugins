9aa891485044f5b6f164d7d6bbcb6369f415651d
Tobias Menzel <t.menzel@sprinteins.com>
2025-12-11 16:01:44 +0100

    refactor: remove unused ConnectionFilter type export from TelemetryView

--- packages/plugins/communication-explorer/src/lib/index.ts
+++ packages/plugins/communication-explorer/src/lib/index.ts
@@ -1,2 +1,1 @@
 export { default as TelemetryView } from '../ui/components/telemetry-view/telemetry-view.svelte'
-export type { ConnectionFilter } from '../ui/components/telemetry-view/connection-filter-utils'
