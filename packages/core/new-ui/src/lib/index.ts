5337a1b2a4819d1f18ff4868d0ff6ea463835b15
Tobias Menzel <t.menzel@sprinteins.com>
2026-01-07 16:01:34 +0100

    feat: use the latest tw4 shadcn version hmm

---
+++ packages/core/new-ui/src/lib/index.ts
@@ -0,0 +1,4 @@
+// Reexport your entry components here
+export * as Button from './components/ui/button/index.ts'
+export * as Select from './components/ui/select/index.ts'
+export * as Separator from './components/ui/separator/index.ts'
