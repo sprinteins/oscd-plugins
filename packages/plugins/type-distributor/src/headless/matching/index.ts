569b9884ae234d1b0dbb1427994d16c79502fe68
Tobias Menzel <t.menzel@sprinteins.com>
2026-01-20 17:28:36 +0100

    feat: implement onSelectBayType function for assigning BayType to SCD Bay with validation and equipment matching

---
+++ packages/plugins/type-distributor/src/headless/matching/index.ts
@@ -0,0 +1,7 @@
+/**
+ * Matching operations for BayType assignment
+ * 
+ * This module provides operations for matching and assigning BayTypes to Bays in the SCD
+ */
+
+export { onSelectBayType } from './onSelectBayType'
