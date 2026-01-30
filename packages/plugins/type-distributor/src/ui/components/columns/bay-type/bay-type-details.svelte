c506a3ae748cacdfc80d67cfd83ea833096c7127
maxschmalenbach <m.schmalenbach@sprinteins.com>
2026-01-30 10:39:29 +0100

    tech(bay-type-details): switch imports

--- packages/plugins/type-distributor/src/ui/components/columns/bay-type/bay-type-details.svelte
+++ packages/plugins/type-distributor/src/ui/components/columns/bay-type/bay-type-details.svelte
@@ -3,7 +3,8 @@ import type {
 	ConductingEquipmentTemplate,
 	FunctionTemplate
 } from '@/headless/common-types'
-  import { EqFunctionType, FunctionType } from '.';
+import EqFunctionType from './eq-function-type.svelte'
+import FunctionType from './function-type.svelte'
 
 const {
 	functionTemplates,
