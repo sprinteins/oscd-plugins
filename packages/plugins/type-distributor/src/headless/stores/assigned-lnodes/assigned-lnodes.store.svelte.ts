3cc9fe4f38ad69e0aa0734dbd980b3dfb9ecbb9e
maxschmalenbach <m.schmalenbach@sprinteins.com>
2026-02-05 17:17:07 +0100

    debugging

--- packages/plugins/type-distributor/src/headless/stores/assigned-lnodes/assigned-lnodes.store.svelte.ts
+++ packages/plugins/type-distributor/src/headless/stores/assigned-lnodes/assigned-lnodes.store.svelte.ts
@@ -9,25 +9,36 @@ class UseAssignedLNodesStore {
 	private buildKey(lnode: LNodeTemplate): LNodeKey {
 		const iedName = lnode.iedName || ''
 		const ldInst = lnode.lDeviceName || ''
-		return `${iedName}:${ldInst}:${lnode.lnClass}:${lnode.lnType}:${lnode.lnInst}`
+		const key = `${iedName}:${ldInst}:${lnode.lnClass}:${lnode.lnType}:${lnode.lnInst}` as LNodeKey
+		console.debug('[assignedLNodesStore] buildKey', {
+			iedName,
+			ldInst,
+			lnClass: lnode.lnClass,
+			lnType: lnode.lnType,
+			lnInst: lnode.lnInst,
+			key,
+		})
+		return key
 	}
 
 	rebuild() {
 		const doc = pluginGlobalStore.xmlDocument
 		if (!doc) {
+			console.debug('[assignedLNodesStore] rebuild: no xmlDocument, clearing index')
 			this.assignedIndex = new Set()
 			return
 		}
 
 		const index = new Set<LNodeKey>()
 		const lDevices = doc.querySelectorAll('IED > AccessPoint > Server > LDevice')
+		console.debug('[assignedLNodesStore] rebuild: lDevices found', lDevices.length)
 
 		for (const lDevice of lDevices) {
 			// Get IED name and LDevice instance
 			const ied = lDevice.closest('IED')
 			const iedName = ied?.getAttribute('name') || ''
 			const ldInst = lDevice.getAttribute('inst') || ''
-
+		
 			const lnElements = Array.from(lDevice.children).filter(
 				(child) => child.localName === 'LN' || child.localName === 'LN0'
 			)
@@ -39,24 +50,39 @@ class UseAssignedLNodesStore {
 
 				if (lnClass && lnType) {
 					const key = `${iedName}:${ldInst}:${lnClass}:${lnType}:${lnInst}` as LNodeKey
+					console.debug('[assignedLNodesStore] rebuild: add key', {
+						iedName,
+						ldInst,
+						lnClass,
+						lnType,
+						lnInst,
+						key,
+					})
 					index.add(key)
 				}
 			}
 		}
 
+		console.debug('[assignedLNodesStore] rebuild: index size', index.size)
 		this.assignedIndex = index
 	}
 
 	markAsAssigned(lNodes: LNodeTemplate[]) {
+		console.debug('[assignedLNodesStore] markAsAssigned: input size', lNodes.length)
 		const newIndex = new Set(this.assignedIndex)
 		for (const lnode of lNodes) {
-			newIndex.add(this.buildKey(lnode))
+			const key = this.buildKey(lnode)
+			console.debug('[assignedLNodesStore] markAsAssigned: add key', key)
+			newIndex.add(key)
 		}
 		this.assignedIndex = newIndex
 	}
 
 	isAssigned(lnode: LNodeTemplate): boolean {
-		return this.assignedIndex.has(this.buildKey(lnode))
+		const key = this.buildKey(lnode)
+		const assigned = this.assignedIndex.has(key)
+		console.debug('[assignedLNodesStore] isAssigned', { key, assigned })
+		return assigned
 	}
 }
 
