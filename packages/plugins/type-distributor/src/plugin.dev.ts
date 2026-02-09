2e1cb4e255de9f7858cf8834c65aaa6fc57c8ef2
Tobias Menzel <t.menzel@sprinteins.com>
2026-02-09 10:40:09 +0100

    refactor(dist): add XMLEditor instance to plugin mount

--- packages/plugins/type-distributor/src/plugin.dev.ts
+++ packages/plugins/type-distributor/src/plugin.dev.ts
@@ -1,6 +1,7 @@
 import { mount } from 'svelte'
 import Plugin from './plugin.svelte'
 import { sclMockA } from '@oscd-plugins/core-api/mocks/v1'
+import { XMLEditor } from '@openscd/oscd-editor'
 
 mount(Plugin, {
 	target: document.getElementById('plugin') as Element,
@@ -8,6 +9,7 @@ mount(Plugin, {
 		doc: new DOMParser().parseFromString(sclMockA, 'text/xml'),
 		docName: 'scl-mock-A',
 		editCount: 0,
+		editor: new XMLEditor(),
 		locale: 'en',
 		isCustomInstance: false
 	}
