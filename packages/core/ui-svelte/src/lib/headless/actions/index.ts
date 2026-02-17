ae372c8b50e1b265a8859090cfffd701ebefba78
maxschmalenbach <m.schmalenbach@sprinteins.com>
2026-02-17 09:56:01 +0100

    tech(useInitScdTemplate): create tempalte and integrate to prevent load ssd

--- packages/core/ui-svelte/src/lib/headless/actions/index.ts
+++ packages/core/ui-svelte/src/lib/headless/actions/index.ts
@@ -1,4 +1,5 @@
 export * from './useInitPlugin.svelte.js'
 export * from './useInitSsdTemplate.svelte.js'
+export * from './useInitScdTemplate.svelte.js'
 export * from './useClickOutside.svelte.js'
 export * from './useRemoveInstanceWarningAndIssueToast.svelte.js'
