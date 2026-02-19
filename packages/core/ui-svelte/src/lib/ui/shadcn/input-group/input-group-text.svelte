925ef9408f743c69b0a4c8bb4aed7716a9bc8729
maxschmalenbach <m.schmalenbach@sprinteins.com>
2026-02-19 16:28:23 +0100

    tech(input-group): added shadcn comp

---
+++ packages/core/ui-svelte/src/lib/ui/shadcn/input-group/input-group-text.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import type { WithElementRef } from "bits-ui";
+  import { cn } from "$lib/utils/shadcn.js";
+  import type { HTMLAttributes } from "svelte/elements";
+  let {
+    ref = $bindable(null),
+    class: className,
+    children,
+    ...restProps
+  }: WithElementRef<HTMLAttributes<HTMLSpanElement>> = $props();
+</script>
+
+<span
+  bind:this={ref}
+  class={cn(
+    "text-muted-foreground flex items-center gap-2 text-sm [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4",
+    className
+  )}
+  {...restProps}
+>
+  {@render children?.()}
+</span>
