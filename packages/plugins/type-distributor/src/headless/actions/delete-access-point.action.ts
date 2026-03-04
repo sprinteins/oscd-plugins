1b015b13b7aa37d5271a8c0d0688bfa238f11a3e
maxschmalenbach <m.schmalenbach@sprinteins.com>
2026-03-04 11:28:14 +0100

    tech(access-point, ied-lnode): move delete into actions

---
+++ packages/plugins/type-distributor/src/headless/actions/delete-access-point.action.ts
@@ -0,0 +1,38 @@
+import { buildEditsForDeleteAccessPoint } from '../scl/edits/delete-elements'
+import { bayStore } from '../stores'
+import { getEditor } from '../utils'
+
+type deleteAccessPointFromIedParams = {
+	iedName: string
+	accessPoint: Element
+	hasLNodes: boolean
+}
+
+export function deleteAccessPointFromIed({
+	iedName,
+	accessPoint,
+	hasLNodes
+}: deleteAccessPointFromIedParams): void {
+	const editor = getEditor()
+
+	if (hasLNodes && !bayStore.scdBay) {
+		console.error(
+			'[AccessPoint] No bay selected - required to clear LNode references'
+		)
+		return
+	}
+	const edits = buildEditsForDeleteAccessPoint({
+		accessPoint,
+		iedName,
+		selectedBay: bayStore.scdBay
+	})
+	if (!(edits.length > 0)) {
+		console.warn(
+			'[AccessPoint] No edits generated for deleting AccessPoint - check if it still exists'
+		)
+		return
+	}
+	editor.commit(edits, {
+		title: `Delete AccessPoint ${accessPoint.getAttribute('name') ?? '(unnamed)'} from ${iedName}`
+	})
+}
