6f5cf0141c351f79038aec7feb4d5fc22cff709c
maxschmalenbach <m.schmalenbach@sprinteins.com>
2026-03-04 11:10:56 +0100

    tech(build-edits): add warning, query access points

--- packages/plugins/type-distributor/src/headless/scl/edits/delete-elements/build-edits-for-delete-empty-ied.ts
+++ packages/plugins/type-distributor/src/headless/scl/edits/delete-elements/build-edits-for-delete-empty-ied.ts
@@ -1,16 +1,19 @@
 import type { Remove } from '@openscd/oscd-api'
 import { getDocument } from '@/headless/utils'
-import { queryIedElement } from '../../queries'
+import { queryAccessPointsFromIed, queryIedElement } from '../../queries'
 
 export function buildEditsForDeleteEmptyIed(iedName: string): Remove[] {
 	const doc = getDocument()
 	const iedElement = queryIedElement(doc, iedName)
 
 	if (!iedElement) {
+		console.warn(`IED with name "${iedName}" not found.`)
 		return []
 	}
 
-	if (iedElement.children.length > 0) {
+	const hasAccessPoints = queryAccessPointsFromIed(doc, iedName).length > 0
+	if (hasAccessPoints) {
+		console.warn(`IED with name "${iedName}" is not empty.`)
 		return []
 	}
 
