9f082c8dcad175b3ed0cf75bf9dbfa1240e199e7
maxschmalenbach <m.schmalenbach@sprinteins.com>
2026-03-24 12:25:27 +0100

    fix(dialog-trigger): overwrite overflow hidden

--- packages/plugins/type-distributor/src/ui/components/columns/s-ied/create-ied-ap-dialog/add-ied-ap-dialog-trigger.svelte
+++ packages/plugins/type-distributor/src/ui/components/columns/s-ied/create-ied-ap-dialog/add-ied-ap-dialog-trigger.svelte
@@ -11,3 +11,9 @@ async function handleOpenDialog() {
 </script>
 
 <Button.Root class="w-full" onclick={handleOpenDialog}>Add</Button.Root>
+
+<style lang="scss">
+:global(.dialog-box) {
+	overflow-y: scroll !important;
+}
+</style>
