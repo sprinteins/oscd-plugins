0d7912deb6bf6feb50326ad670064b0730ad54ad
rds <rasul@futureblur.com>
2026-08-24 13:12:21 +0200

    fix: playwright startup error

--- packages/plugins/type-designer/playwright.config.ts
+++ packages/plugins/type-designer/playwright.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
 		['html', { open: 'never' }],
 	],
 	use: {
-		baseURL: 'http://127.0.0.1:4175',
+		baseURL: 'http://localhost:4175',
 		trace: 'on-first-retry',
 		screenshot: 'only-on-failure',
 		video: 'retain-on-failure',
@@ -31,8 +31,8 @@ export default defineConfig({
 	],
 
 	webServer: {
-		command: 'pnpm dev -- --host 127.0.0.1',
-		url: 'http://127.0.0.1:4175',
+		command: 'pnpm run dev',
+		port: 4175,
 		reuseExistingServer: !process.env.CI,
 		timeout: 120_000,
 	},
